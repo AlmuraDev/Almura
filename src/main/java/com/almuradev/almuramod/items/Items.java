3b5dfc1d9a696060622259d2524bdff643854fc2
Mike Howe <mike@mcsnetworks.com>
2014-09-09 23:39:05 -0500

    Code to setup creative tabs for Almura Custom stuffs.
    Signed-off-by: Mike Howe <mike@mcsnetworks.com>

--- src/main/java/com/almuradev/almuramod/items/Items.java
+++ src/main/java/com/almuradev/almuramod/items/Items.java
@@ -1,6 +1,29 @@
 package com.almuradev.almuramod.items;
 
+import net.minecraft.item.Item;
+
 public class Items {
 
+    // Fake items for Creative Menu Tabs 
+    public static final Item ALMURA_ROOFING = new AlmuraItem("almuraRoofing", false);
+    public static final Item ALMURA_LIGHTING = new AlmuraItem("almuraLighting", false);
+    public static final Item ALMURA_FURNITURE = new AlmuraItem("almuraFurniture", false);
+    public static final Item ALMURA_BUILDING = new AlmuraItem("almuraBuilding", false);
+    public static final Item ALMURA_DECORATION = new AlmuraItem("almuraDecoration", false);
+    public static final Item ALMURA_STORAGE = new AlmuraItem("almuraStorage", false);
+    public static final Item ALMURA_BARRELS = new AlmuraItem("almuraBarrels", false);
+    public static final Item ALMURA_FLOWERS = new AlmuraItem("almuraFlowers", false);
+    public static final Item ALMURA_PLANTS = new AlmuraItem("almuraPlants", false);
+    public static final Item ALMURA_CROPS = new AlmuraItem("almuraCrops", false);
+    public static final Item ALMURA_FLAGS = new AlmuraItem("almuraFlags", false);
+    public static final Item ALMURA_SIGNS = new AlmuraItem("almuraSigns", false);
+    public static final Item ALMURA_LETTERS = new AlmuraItem("almuraLetters", false);
+    public static final Item ALMURA_ORES = new AlmuraItem("almuraOres", false);
+    public static final Item ALMURA_FOOD = new AlmuraItem("almuraFood", false);
+    public static final Item ALMURA_DRINK = new AlmuraItem("almuraDrink", false);
+    public static final Item ALMURA_INGREDIENTS = new AlmuraItem("almuraIngredient", false);
+    public static final Item ALMURA_BOTTLES = new AlmuraItem("almuraBottles", false);
+    public static final Item ALMURA_TOOLS = new AlmuraItem("almuraTools", false);
+    
     public static void fakeStaticLoad() {}
 }
