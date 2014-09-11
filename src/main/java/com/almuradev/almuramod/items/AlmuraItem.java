fc1f3864ad5e90912e1d58d0daa74478c2d22f92
Mike Howe <mike@mcsnetworks.com>
2014-09-10 20:19:01 -0500

    Constructor changes for items.
    Signed-off-by: Mike Howe <mike@mcsnetworks.com>

--- src/main/java/com/almuradev/almuramod/items/AlmuraItem.java
+++ src/main/java/com/almuradev/almuramod/items/AlmuraItem.java
@@ -1,13 +1,26 @@
 package com.almuradev.almuramod.items;
 
 import com.almuradev.almuramod.AlmuraMod;
+
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 
 public class AlmuraItem extends Item {
-    public AlmuraItem(String name, boolean showInCreativeTab) {
+   
+    // Fake Item Registry
+    public AlmuraItem(String name) {
+        registerAlmuraItem(name, false, null);
+    }
+    
+    // Real Item Registry
+    public AlmuraItem(String name, boolean showInCreativeTab, CreativeTabs creativeTabName) {
+        registerAlmuraItem(name, showInCreativeTab, creativeTabName);
+    }
+    
+    public void registerAlmuraItem(String name, boolean showInCreativeTab, CreativeTabs creativeTabName) {
         if (showInCreativeTab) {
-            setCreativeTab(AlmuraMod.act_Building);
+            setCreativeTab(creativeTabName);
         }
 
         GameRegistry.registerItem(this, name);
