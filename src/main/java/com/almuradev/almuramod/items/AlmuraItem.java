1a1f4bce803d4067e81756760088ea1e17c6ed3c
Mike Howe <mike@mcsnetworks.com>
2014-09-10 22:06:55 -0500

    Start of Register Icons.
    Signed-off-by: Mike Howe <mike@mcsnetworks.com>

--- src/main/java/com/almuradev/almuramod/items/AlmuraItem.java
+++ src/main/java/com/almuradev/almuramod/items/AlmuraItem.java
@@ -3,7 +3,11 @@ package com.almuradev.almuramod.items;
 import com.almuradev.almuramod.AlmuraMod;
 
 import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.util.IIcon;
 import net.minecraft.item.Item;
 
 public class AlmuraItem extends Item {
@@ -23,7 +27,18 @@ public class AlmuraItem extends Item {
             setCreativeTab(creativeTabName);
         }
         this.setUnlocalizedName(name);
-        setTextureName(AlmuraMod.MOD_ID.toLowerCase() + ":" + name);
+        setTextureName(AlmuraMod.MOD_ID.toLowerCase() + ":" + name);     
         GameRegistry.registerItem(this, name);
+    }
+    
+    @SideOnly(Side.CLIENT)
+    private IIcon[] icons;
+    
+    @SideOnly(Side.CLIENT)
+    public void registerIcons(IIconRegister par1IconRegister) {
+        icons = new IIcon[2]; 
+        for (int i = 0; i < icons.length; i++) {
+            icons[i] = par1IconRegister.registerIcon(AlmuraMod.MOD_ID + ":" + (this.getUnlocalizedName().substring(5)));
+        }
     }
 }
