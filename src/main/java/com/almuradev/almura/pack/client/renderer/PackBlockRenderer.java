84b930bfcfcb3be95af6b5e44818cc1c18487df9
Mike Howe <mike@mcsnetworks.com>
2014-11-15 21:37:36 -0600

    Cleanup
    Signed-off-by: Mike Howe <mike@mcsnetworks.com>

--- src/main/java/com/almuradev/almura/pack/client/renderer/PackBlockRenderer.java
+++ src/main/java/com/almuradev/almura/pack/client/renderer/PackBlockRenderer.java
@@ -6,7 +6,6 @@
 package com.almuradev.almura.pack.client.renderer;
 
 import com.almuradev.almura.Almura;
-import com.almuradev.almura.client.gui.AlmuraMainMenu;
 import com.almuradev.almura.pack.IClipContainer;
 import com.almuradev.almura.pack.IShapeContainer;
 import com.almuradev.almura.pack.PackUtil;
@@ -18,17 +17,10 @@ import net.malisis.core.renderer.element.Face;
 import net.malisis.core.renderer.element.Shape;
 import net.malisis.core.renderer.element.shape.Cube;
 import net.minecraft.block.Block;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.ItemRenderer;
-import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.RenderHelper;
-import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.item.Item;
 import net.minecraft.util.IIcon;
 import net.minecraftforge.common.util.ForgeDirection;
-import org.lwjgl.opengl.GL11;
-
-import java.util.Random;
 
 public class PackBlockRenderer extends BaseRenderer {
     private Cube vanillaShape;
