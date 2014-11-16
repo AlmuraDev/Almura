5d757902bb7d5f1c1332198e5e92c4a969cf2312
Mike Howe <mike@mcsnetworks.com>
2014-11-15 22:07:35 -0600

    Additions.
    Signed-off-by: Mike Howe <mike@mcsnetworks.com>

--- src/main/java/com/almuradev/almura/pack/crop/PackCrop.java
+++ src/main/java/com/almuradev/almura/pack/crop/PackCrop.java
@@ -1,6 +1,11 @@
 package com.almuradev.almura.pack.crop;
 
-public class PackCrop {
+import java.util.Random;
+
+import net.minecraft.block.BlockCrops;
+import net.minecraft.world.World;
+
+public class PackCrop extends BlockCrops {
     // Constructor Object Needs:
     //  a. seed
     //  b. amount of stages (int)
@@ -16,4 +21,16 @@ public class PackCrop {
     //  l. fertilizer type (itemStack)
     //  m. damage player
     //  n. drop seed on grass break (boolean)
+    
+    @Override
+    public void updateTick(World world, int x, int y, int z, Random random) {
+        // Todo: what to do in update tick
+    }
+    
+    private float getGrowthRate(World world, int x, int y, int z) {        
+        // Todo: determine growth rate.
+        return 0;        
+    }
+    
+    //Todo:  many many more methods needed here.
 }
