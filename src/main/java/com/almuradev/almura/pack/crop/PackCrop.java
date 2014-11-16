8538bdef78aa120a99798a834bb00167269097a0
Mike Howe <mike@mcsnetworks.com>
2014-11-15 22:02:02 -0600

    Start of Crops
    Signed-off-by: Mike Howe <mike@mcsnetworks.com>

---
+++ src/main/java/com/almuradev/almura/pack/crop/PackCrop.java
@@ -0,0 +1,19 @@
+package com.almuradev.almura.pack.crop;
+
+public class PackCrop {
+    // Constructor Object Needs:
+    //  a. seed
+    //  b. amount of stages (int)
+    //  c. minimum light to grow
+    //  d. maximum light to grow
+    //  e. growth rate
+    //  f. drop type (itemStack)
+    //  g. drop quantity (int)
+    //  h. drop bonus (itemStack)
+    //  i. bonus quantity (int)
+    //  j. water required to grow (boolean)
+    //  k. water range scan (int)
+    //  l. fertilizer type (itemStack)
+    //  m. damage player
+    //  n. drop seed on grass break (boolean)
+}
