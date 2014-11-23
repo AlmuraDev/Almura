c66f07a0f780d71aa94ca943a38fcfde34264318
Chris Sanders <zidane@outlook.com>
2014-11-22 20:38:40 -0600

    Update for MalisisCore changes
    Signed-off-by: Chris Sanders <zidane@outlook.com>

--- src/main/java/com/almuradev/almura/pack/model/PackModel.java
+++ src/main/java/com/almuradev/almura/pack/model/PackModel.java
@@ -5,23 +5,28 @@
  */
 package com.almuradev.almura.pack.model;
 
-import com.almuradev.almura.Almura;
+import com.almuradev.almura.Filesystem;
 import com.almuradev.almura.pack.ContentPack;
 import com.flowpowered.cerealization.config.ConfigurationException;
 import net.malisis.core.renderer.model.MalisisModel;
-import net.minecraft.util.ResourceLocation;
+import net.malisis.core.renderer.model.loader.ObjFileImporter;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class PackModel extends MalisisModel {
-    private static final PackObjFileImporter IMPORTER = new PackObjFileImporter();
     private final String name;
 
-    public PackModel(String name, ResourceLocation location) {
-        super(location, IMPORTER);
+    public PackModel(String name, InputStream stream) {
+        super(new ObjFileImporter(stream));
         this.name = name;
     }
 
-    public static PackModel createFromReader(ContentPack pack, String name) throws ConfigurationException {
-        return new PackModel(name, new ResourceLocation(Almura.MOD_ID, "packs/" + pack.getName() + "//" + name + ".obj"));
+    public static PackModel createFromReader(ContentPack pack, String name) throws ConfigurationException, IOException {
+        return new PackModel(name, Files.newInputStream(Paths.get(Filesystem.CONFIG_PACKS_PATH.toString(), pack.getName() + File.separator + name + ".obj")));
     }
 
     public String getName() {
