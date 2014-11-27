b426e73330d4c79d8ba1ffb801c7683bb73293a2
Chris Sanders <zidane@outlook.com>
2014-11-26 21:58:30 -0600

    Fix MixinGL11
    Signed-off-by: Chris Sanders <zidane@outlook.com>

--- src/main/java/com/almuradev/almura/core/AlmuraCoreMod.java
+++ src/main/java/com/almuradev/almura/core/AlmuraCoreMod.java
@@ -2,12 +2,23 @@ package com.almuradev.almura.core;
 
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
 import net.minecraft.launchwrapper.Launch;
+import net.minecraft.launchwrapper.LaunchClassLoader;
 
+import java.lang.reflect.Field;
 import java.util.Map;
+import java.util.Set;
 
 public class AlmuraCoreMod implements IFMLLoadingPlugin {
     public AlmuraCoreMod() {
         Launch.classLoader.addClassLoaderExclusion("com.almuradev.almura.core.mixin.transformer.");
+        try {
+            Field f = LaunchClassLoader.class.getDeclaredField("classLoaderExceptions");
+            f.setAccessible(true);
+            Set<String> classLoaderExclusions = (Set<String>) f.get(Launch.classLoader);
+            classLoaderExclusions.remove("org.lwjgl.");
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
     @Override
     public String[] getASMTransformerClass() {
--- src/main/java/com/almuradev/almura/core/mixin/impl/MixinGL11.java
+++ src/main/java/com/almuradev/almura/core/mixin/impl/MixinGL11.java
@@ -1,5 +1,6 @@
 package com.almuradev.almura.core.mixin.impl;
 
+import com.almuradev.almura.core.mixin.Shadow;
 import org.lwjgl.BufferChecks;
 import org.lwjgl.opengl.ContextCapabilities;
 import org.lwjgl.opengl.GLContext;
@@ -8,19 +9,37 @@ import org.lwjgl.opengl.GL11;
 import com.almuradev.almura.core.mixin.Mixin;
 import com.almuradev.almura.core.mixin.Overwrite;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 @Mixin(GL11.class)
-public abstract class MixinGL11 extends GL11 {
-	
+public abstract class MixinGL11 {
+	private static Field f;
 	private static int cachedId;
+
+	@Shadow
+	static native void nglBindTexture(int target, int texture, long function_pointer);
 
 	@Overwrite
 	public static void glBindTexture(int target, int texture) {
+		if (f == null) {
+			try {
+				f = ContextCapabilities.class.getDeclaredField("glBindTexture");
+			} catch (NoSuchFieldException e) {
+				e.printStackTrace();
+			}
+		}
 		if (texture != cachedId) {
 			ContextCapabilities caps = GLContext.getCapabilities();
-			long function_pointer = caps.glBindTexture;
-			BufferChecks.checkFunctionAddress(function_pointer);
-			nglBindTexture(target, texture, function_pointer);
-			cachedId = texture;
+			f.setAccessible(true);
+			try {
+				long function_pointer = f.getLong(caps);
+				BufferChecks.checkFunctionAddress(function_pointer);
+				nglBindTexture(target, texture, function_pointer);
+				cachedId = texture;
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
