ecad2ba457840da921a6796e97e1e7ffef78202e
Chris Sanders <zidane@outlook.com>
2014-12-01 15:46:31 -0600

    Fix player list
    Signed-off-by: Chris Sanders <zidane@outlook.com>

--- src/main/java/com/almuradev/almura/client/gui/ingame/IngameHUD.java
+++ src/main/java/com/almuradev/almura/client/gui/ingame/IngameHUD.java
@@ -385,7 +385,7 @@ public class IngameHUD extends AlmuraGui {
         if (MINECRAFT.isSingleplayer() && firstPass) {
             serverCount.setText("--");
         } else {
-            if (playerCount != MINECRAFT.getNetHandler().playerInfoList.size()) {
+            if ((MINECRAFT.getIntegratedServer().getPublic() || !MINECRAFT.isSingleplayer()) && playerCount != MINECRAFT.getNetHandler().playerInfoList.size()) {
                 playerCount = MINECRAFT.getNetHandler().playerInfoList.size();
 
                 serverCount.setText(playerCount + "/" + MINECRAFT.getNetHandler().currentServerMaxPlayers);
