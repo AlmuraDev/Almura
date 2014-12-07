624ee16ad123361d256dc1059a9e3491de706d12
Chris Sanders <zidane@outlook.com>
2014-12-07 14:00:26 -0600

    Only stitch textures once
    Signed-off-by: Chris Sanders <zidane@outlook.com>

--- src/main/java/com/almuradev/almura/pack/PackCreator.java
+++ src/main/java/com/almuradev/almura/pack/PackCreator.java
@@ -12,16 +12,24 @@ import com.almuradev.almura.pack.block.PackBlock;
 import com.almuradev.almura.pack.crop.PackCrops;
 import com.almuradev.almura.pack.crop.PackSeeds;
 import com.almuradev.almura.pack.crop.stage.Stage;
+import com.almuradev.almura.pack.crop.stage.property.CollisionProperty;
 import com.almuradev.almura.pack.crop.stage.property.HydrationProperty;
+import com.almuradev.almura.pack.crop.stage.property.LightProperty;
+import com.almuradev.almura.pack.crop.stage.property.source.CollisionSource;
 import com.almuradev.almura.pack.crop.stage.property.source.HydrationSource;
+import com.almuradev.almura.pack.crop.stage.property.source.RangeSource;
 import com.almuradev.almura.pack.item.PackFood;
 import com.almuradev.almura.pack.item.PackItem;
 import com.flowpowered.cerealization.config.ConfigurationException;
 import com.flowpowered.cerealization.config.ConfigurationNode;
 import com.flowpowered.cerealization.config.yaml.YamlConfiguration;
 import com.google.common.collect.Lists;
+import cpw.mods.fml.common.registry.EntityRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityList;
+import net.minecraft.item.Item;
 import org.apache.commons.lang3.StringEscapeUtils;
 
 import java.util.Arrays;
@@ -143,43 +151,93 @@ public class PackCreator {
         return new PackSeeds(pack, crop.getUnlocalizedName() + "\\", textureName, showInCreativeTab, creativeTabName, crop, soil);
     }
 
-    public static Stage createStageFromNode(int id, String name, ConfigurationNode node) {
+    public static Stage createStageFromNode(ContentPack pack, PackCrops crop, int id, ConfigurationNode node) {
         final Map<Integer, List<Integer>> textureCoordinatesByFace = PackUtil.extractCoordsFrom(node.getChild("coords"));
         String shapeName = node.getChild("shape").getString();
         if (shapeName != null) {
             shapeName = shapeName.split(".shape")[0];
         }
+
+        //HYDRATION
         final ConfigurationNode hydrationNode = node.getNode("hydration");
         final boolean hydrationEnabled = hydrationNode.getChild("enabled").getBoolean(false);
         final ConfigurationNode hydrationSourcesNode = hydrationNode.getNode("sources");
         final List<HydrationSource> hydrationSources = Lists.newArrayList();
-        final HydrationProperty hydrationProperty;
-
         if (hydrationSourcesNode != null) {
             for (Map.Entry<String, ConfigurationNode> entry : hydrationSourcesNode.getChildren().entrySet()) {
                 final String[] split = entry.getKey().split(StringEscapeUtils.escapeJava("\\"));
                 if (split.length == 1) {
-                    Almura.LOGGER.warn("An invalid hydration source was provided [" + entry.getKey() + "].");
+                    Almura.LOGGER.warn("An invalid hydration source was provided [" + entry.getKey() + "] for.");
                     continue;
                 }
-                String identifier = split[1];
-                if (split.length > 2) {
-                    for (int i = 1; i < split.length; ++i) {
-                        identifier = identifier + "\\" + split[i];
-                    }
-                }
-                final Block block = GameRegistry.findBlock(split[0], identifier);
-                if (block == null) {
+                final String identifier = parseIdentifier(split);
+                final Block hydrationSource = getFromRegistry(split[0], identifier, Block.class);
+                if (hydrationSource == null) {
                     Almura.LOGGER.warn("Could not find block [" + identifier + "] provided by mod [" + split[0] + "] for stage [" + id + "] in crop ["
-                                       + name + "].");
+                                       + crop.getIdentifier() + "] in pack [" + pack.getName() + "].");
                     continue;
                 }
                 final int neededProximity = entry.getValue().getChild("needed-proximity").getInt(6);
-                hydrationSources.add(new HydrationSource(block, neededProximity));
+                hydrationSources.add(new HydrationSource(hydrationSource, neededProximity));
             }
-            hydrationProperty = new HydrationProperty(hydrationEnabled, hydrationSources.toArray(new HydrationSource[hydrationSources.size()]));
-        } else {
-            hydrationProperty = new HydrationProperty(false, null);
+        }
+        final HydrationProperty hydrationProperty = new HydrationProperty(hydrationEnabled, hydrationSources.toArray(new HydrationSource[hydrationSources.size()]));
+
+        //LIGHT
+        final ConfigurationNode lightNode = node.getNode("light");
+        final int lightLevel = lightNode.getChild("emission").getInt(0);
+        final int lightOpacity = lightNode.getChild("opacity").getInt(0);
+        final ConfigurationNode lightRequiredNode = lightNode.getNode("required");
+        final boolean lightRequiredEnabled = lightRequiredNode.getBoolean(true);
+        final int lightRequiredMin = lightRequiredNode.getChild("min").getInt(0);
+        final int lightRequiredMax = lightRequiredNode.getChild("max").getInt(15);
+        final LightProperty lightProperty = new LightProperty(lightLevel, lightOpacity, new RangeSource(lightRequiredEnabled, lightRequiredMin, lightRequiredMax));
+
+        //COLLISION
+        final ConfigurationNode collisionNode = node.getNode("collision");
+        final boolean collisionEnabled = collisionNode.getChild("enabled").getBoolean(false);
+        final ConfigurationNode collisionSourcesNode = collisionNode.getNode("sources");
+        final List<CollisionSource> collisionSources = Lists.newArrayList();
+        if (collisionSourcesNode != null) {
+            for (Map.Entry<String, ConfigurationNode> collisionSource : collisionSourcesNode.getChildren().entrySet()) {
+                final String[] entityIdentifierSplit = collisionSource.getKey().split(StringEscapeUtils.escapeJava("\\"));
+                String entityIdentifier = entityIdentifierSplit[0];
+                if (entityIdentifierSplit.length > 1) {
+                    for (int i = 1; i < entityIdentifierSplit.length; i++) {
+                        entityIdentifier = entityIdentifier + "." + entityIdentifierSplit[i];
+                    }
+                }
+                final Class<? extends Entity> entityClazz = (Class<? extends Entity>) EntityList.stringToClassMapping.get(entityIdentifier);
+                if (entityClazz == null) {
+                    Almura.LOGGER.warn("Unknown collision source [" + collisionSource.getKey() + "] provided for stage [" + id + "] in crop [" + crop
+                            .getIdentifier() + "] in pack [" + pack.getName() + "].");
+                }
+                final float healthChange = collisionSourcesNode.getChild(collisionSource.getKey()).getFloat(0f);
+                collisionSources.add(new CollisionSource(entityClazz, healthChange));
+            }
+        }
+        final CollisionProperty collisionProperty = new CollisionProperty(collisionEnabled, collisionSources.toArray(new CollisionSource[collisionSources.size()]));
+
+        return null;
+    }
+
+    private static String parseIdentifier(String[] split) {
+        String identifier = split[1];
+        if (split.length > 2) {
+            for (int i = 1; i < split.length; ++i) {
+                identifier = identifier + "\\" + split[i];
+            }
+        }
+        return identifier;
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T> T getFromRegistry(String modid, String identifier, Class<T> type) {
+
+        if (type == Block.class) {
+            return (T) GameRegistry.findBlock(modid, identifier);
+        } else if (type == Item.class) {
+            return (T) GameRegistry.findItem(modid, identifier);
         }
 
         return null;
--- src/main/java/com/almuradev/almura/pack/renderer/PackIcon.java
+++ src/main/java/com/almuradev/almura/pack/renderer/PackIcon.java
@@ -28,7 +28,7 @@ public class PackIcon extends MalisisIcon {
     private final IPackObject object;
 
     public PackIcon(IPackObject object, String textureName) {
-        super(Almura.MOD_ID.toLowerCase() + ":packs/" + object.getPack().getName() + "/" + textureName);
+        super(Almura.MOD_ID.toLowerCase() + ":" + textureName);
         this.object = object;
     }
 
@@ -39,63 +39,52 @@ public class PackIcon extends MalisisIcon {
 
     @Override
     public boolean load(IResourceManager manager, ResourceLocation location) {
-        String path = location.getResourcePath();
-        //Hack to remove forced item prefix
-        //TODO Figure out how to make this un-necessary one day...
-        String[] split = location.getResourcePath().split("textures/items/");
-        if (split.length > 1) {
-            path = split[1];
-        }
+        String textureName = location.getResourcePath();
 
-        if (path.startsWith("packs")) {
-            final String[] tokens = path.split("/");
-            final String packName = tokens[1];
-            final String textureName = tokens[2];
-            final Path texturePath = Paths.get(Filesystem.CONFIG_PATH.toString(), "images" + File.separator + textureName + ".png");
+        final Path texturePath = Paths.get(Filesystem.CONFIG_PATH.toString(), "images" + File.separator + textureName + ".png");
 
-            final int mipmapLevels = Minecraft.getMinecraft().gameSettings.mipmapLevels;
-            final boolean anisotropic = Minecraft.getMinecraft().gameSettings.anisotropicFiltering > 1.0F;
+        final int mipmapLevels = Minecraft.getMinecraft().gameSettings.mipmapLevels;
+        final boolean anisotropic = Minecraft.getMinecraft().gameSettings.anisotropicFiltering > 1.0F;
 
-            try {
-                BufferedImage[] textures = new BufferedImage[1 + mipmapLevels];
-                textures[0] = ImageIO.read(Files.newInputStream(texturePath));
-                loadSprite(textures, null, anisotropic);
-                return false;
-            } catch (RuntimeException e) {
-                if (e.getMessage().equalsIgnoreCase("broken aspect ratio and not an animation")) {
-                    if (Configuration.DEBUG_MODE || Configuration.DEBUG_PACKS_MODE) {
-                        Almura.LOGGER
-                                .error("Failed to load icon [" + textureName + ".png] used by [" + object.getIdentifier() + "] requested by pack ["
-                                       + packName + "]. Aspect ratio is broken, make sure it is a power of 2 and has an equivalent width and height.",
-                                       e);
-                    } else {
-                        Almura.LOGGER.warn(
-                                "Failed to load icon [" + textureName + ".png] used by [" + object.getIdentifier() + "] requested by pack ["
-                                + packName + "]. Aspect ratio is broken, make sure it is a power of 2 and has an equivalent width and height.");
-                    }
-                } else {
-                    if (Configuration.DEBUG_MODE || Configuration.DEBUG_PACKS_MODE) {
-                        Almura.LOGGER
-                                .error("Failed to load icon [" + textureName + ".png] used by [" + object.getIdentifier() + "] requested by pack ["
-                                       + packName + "]", e);
-                    } else {
-                        Almura.LOGGER.warn(
-                                "Failed to load icon [" + textureName + ".png] used by [" + object.getIdentifier() + "] requested by pack ["
-                                + packName + "]");
-                    }
-                }
-            } catch (IOException e1) {
+        try {
+            BufferedImage[] textures = new BufferedImage[1 + mipmapLevels];
+            textures[0] = ImageIO.read(Files.newInputStream(texturePath));
+            loadSprite(textures, null, anisotropic);
+            return false;
+        } catch (RuntimeException e) {
+            if (e.getMessage().equalsIgnoreCase("broken aspect ratio and not an animation")) {
                 if (Configuration.DEBUG_MODE || Configuration.DEBUG_PACKS_MODE) {
-                    Almura.LOGGER.error("Failed to load icon [" + textureName + ".png] used by [" + object.getIdentifier() + "] requested by pack ["
-                                        + packName + "]", e1);
+                    Almura.LOGGER
+                            .error("Failed to load icon [" + textureName + ".png] used by [" + object.getIdentifier() + "] requested by pack ["
+                                   + object.getPack().getName() + "]. Aspect ratio is broken, make sure it is a power of 2 and has an equivalent width and height.",
+                                   e);
                 } else {
                     Almura.LOGGER.warn(
-                            "Failed to load icon [" + textureName + ".png] used by [" + object.getIdentifier() + "] requested by pack [" + packName
-                            + "]");
+                            "Failed to load icon [" + textureName + ".png] used by [" + object.getIdentifier() + "] requested by pack ["
+                            + object.getPack().getName() + "]. Aspect ratio is broken, make sure it is a power of 2 and has an equivalent width and height.");
+                }
+            } else {
+                if (Configuration.DEBUG_MODE || Configuration.DEBUG_PACKS_MODE) {
+                    Almura.LOGGER
+                            .error("Failed to load icon [" + textureName + ".png] used by [" + object.getIdentifier() + "] requested by pack ["
+                                   + object.getPack().getName() + "]", e);
+                } else {
+                    Almura.LOGGER.warn(
+                            "Failed to load icon [" + textureName + ".png] used by [" + object.getIdentifier() + "] requested by pack ["
+                            + object.getPack().getName() + "]");
                 }
             }
-        }
+        } catch (IOException e1) {
+            if (Configuration.DEBUG_MODE || Configuration.DEBUG_PACKS_MODE) {
+                Almura.LOGGER.error("Failed to load icon [" + textureName + ".png] used by [" + object.getIdentifier() + "] requested by pack ["
+                                    + object.getPack().getName() + "]", e1);
+            } else {
+                Almura.LOGGER.warn(
+                        "Failed to load icon [" + textureName + ".png] used by [" + object.getIdentifier() + "] requested by pack [" + object.getPack().getName()
+                        + "]");
+            }
 
+        }
         return true;
     }
 }
