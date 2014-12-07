97700220d78642728256aeec305cc20db29af49c
Chris Sanders <zidane@outlook.com>
2014-12-06 23:47:21 -0600

    Kinda need to apply that BlockWall mixin
    Signed-off-by: Chris Sanders <zidane@outlook.com>

--- src/main/java/com/almuradev/almura/core/mixin/impl/MixinBlockWall.java
+++ src/main/java/com/almuradev/almura/core/mixin/impl/MixinBlockWall.java
@@ -7,11 +7,17 @@ package com.almuradev.almura.core.mixin.impl;
 
 import com.almuradev.almura.core.mixin.Mixin;
 import com.almuradev.almura.core.mixin.Overwrite;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockWall;
+import net.minecraft.block.material.Material;
 import net.minecraft.world.IBlockAccess;
 
 @Mixin(BlockWall.class)
-public abstract class MixinBlockWall {
+public abstract class MixinBlockWall extends Block {
+
+    protected MixinBlockWall(Material p_i45394_1_) {
+        super(p_i45394_1_);
+    }
 
     /**
      * Return whether an adjacent block can connect to a wall.
