69daf873e397976468a73f652c5565eedeaac1eb
Mike Howe <mike@mcsnetworks.com>
2014-12-06 20:49:06 -0600

    Version bump
    Signed-off-by: Mike Howe <mike@mcsnetworks.com>

--- src/main/java/com/almuradev/almura/Almura.java
+++ src/main/java/com/almuradev/almura/Almura.java
@@ -23,7 +23,7 @@ public class Almura {
     public static final Logger LOGGER = LogManager.getLogger(Almura.MOD_ID);
     public static final SimpleNetworkWrapper NETWORK_FORGE = new SimpleNetworkWrapper("AM|FOR");
     public static final SimpleNetworkWrapper NETWORK_BUKKIT = new SimpleNetworkWrapper("AM|BUK");
-    public static String GUI_VERSION = "Almura 2.0 build 18";
+    public static String GUI_VERSION = "Almura 2.0 build 20";
     public static String PACK_VERSION = "Content Pack 1";
 
     @SidedProxy(clientSide = ClientProxy.CLASSPATH, serverSide = ServerProxy.CLASSPATH)
