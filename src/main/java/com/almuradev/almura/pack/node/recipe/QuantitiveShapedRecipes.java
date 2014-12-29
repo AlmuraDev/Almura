312653958bac77ef60b9a5531ab1616b25523b82
Chris Sanders <zidane@outlook.com>
2014-12-28 22:52:47 -0600

    Uh things?
    Signed-off-by: Chris Sanders <zidane@outlook.com>

--- src/main/java/com/almuradev/almura/pack/node/recipe/QuantitiveShapedRecipes.java
+++ src/main/java/com/almuradev/almura/pack/node/recipe/QuantitiveShapedRecipes.java
@@ -1,7 +1,6 @@
 package com.almuradev.almura.pack.node.recipe;
 
 import net.minecraft.inventory.InventoryCrafting;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.ShapedRecipes;
 
