bc45aaf11e91b56f5cc203dfbecd015559ec7a6b
Mike Howe <mike@mcsnetworks.com>
2015-01-01 10:55:13 -0600

    Mixin changes
    Signed-off-by: Mike Howe <mike@mcsnetworks.com>

---
+++ src/main/java/com/almuradev/almura/core/mixin/MixinContainerWorkbench.java
@@ -0,0 +1,31 @@
+package com.almuradev.almura.core.mixin;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.Container;
+import net.minecraft.inventory.ContainerWorkbench;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.InventoryCrafting;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.CraftingManager;
+import net.minecraft.world.World;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(ContainerWorkbench.class)
+public abstract class MixinContainerWorkbench extends Container {
+
+	@Shadow 
+	public InventoryCrafting craftMatrix;
+	@Shadow
+	public IInventory craftResult;
+	@Shadow
+	private World worldObj;
+	
+	@Override
+    public ItemStack slotClick(int p_75144_1_, int p_75144_2_, int p_75144_3_, EntityPlayer p_75144_4_) {
+    	this.craftResult.setInventorySlotContents(0, CraftingManager.getInstance().findMatchingRecipe(this.craftMatrix, this.worldObj));
+    	System.out.println("Forced update to Match");
+    	return super.slotClick(p_75144_1_, p_75144_2_, p_75144_3_, p_75144_4_);
+    }
+}
