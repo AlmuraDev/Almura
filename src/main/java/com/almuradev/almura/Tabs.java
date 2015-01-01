90ebce965ac7cfeba095a9f6ccc1b63eb78a16c7
Mike Howe <mike@mcsnetworks.com>
2015-01-01 12:08:12 -0600

    Additional Image and the Seeds Tab setup.
    Signed-off-by: Mike Howe <mike@mcsnetworks.com>

--- src/main/java/com/almuradev/almura/Tabs.java
+++ src/main/java/com/almuradev/almura/Tabs.java
@@ -37,6 +37,7 @@ public class Tabs {
     private static final Item ITEM_FLOWERS = new AlmuraTabItem("flowers");
     private static final Item ITEM_COMPONENTS = new AlmuraTabItem("components");
     private static final Item ITEM_CROPS = new AlmuraTabItem("crops");
+    private static final Item ITEM_SEEDS = new AlmuraTabItem("seeds");
     private static final Item ITEM_FLAGS = new AlmuraTabItem("flags");
     private static final Item ITEM_SIGNS = new AlmuraTabItem("signs");
     private static final Item ITEM_LETTERS = new AlmuraTabItem("letters");
@@ -57,6 +58,7 @@ public class Tabs {
     public static CreativeTabs TAB_FLOWERS = new AlmuraCreativeTabs("flowers", "Almura Flowers", ITEM_FLOWERS);
     public static CreativeTabs TAB_COMPONENTS = new AlmuraCreativeTabs("components", "Almura Components", ITEM_COMPONENTS);
     public static CreativeTabs TAB_CROPS = new AlmuraCreativeTabs("crops", "Almura Crops", ITEM_CROPS);
+    public static CreativeTabs TAB_SEEDS = new AlmuraCreativeTabs("seeds", "Almura Seeds", ITEM_SEEDS);
     public static CreativeTabs TAB_FLAGS = new AlmuraCreativeTabs("flags", "Almura Flags", ITEM_FLAGS);
     public static CreativeTabs TAB_SIGNS = new AlmuraCreativeTabs("signs", "Almura Signs", ITEM_SIGNS);
     public static CreativeTabs TAB_LETTERS = new AlmuraCreativeTabs("letters", "Almura Letters", ITEM_LETTERS);
