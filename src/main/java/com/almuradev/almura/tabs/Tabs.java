0b19b064afe00b8e2dcb399782b8156157eb404f
Steven Downer <grinch@outlook.com>
2015-01-23 22:11:42 -0600

    Add Tab.TAB_OTHER back
    Signed-off-by: Steven Downer <grinch@outlook.com>

--- src/main/java/com/almuradev/almura/tabs/Tabs.java
+++ src/main/java/com/almuradev/almura/tabs/Tabs.java
@@ -52,6 +52,7 @@ public class Tabs {
     public static CreativeTabs TAB_DRINK = new AlmuraCreativeTabs("drinks", "Almura Drinks", ITEM_DRINKS);
     public static CreativeTabs TAB_INGREDIENTS = new AlmuraCreativeTabs("ingredients", "Almura Ingredients", ITEM_INGREDIENTS);
     public static CreativeTabs TAB_TOOLS = new AlmuraCreativeTabs("tool", "Almura Tools", ITEM_TOOL);
+    public static CreativeTabs TAB_OTHER = new AlmuraCreativeTabs("other", "Almura Other", null);
 
     public static void fakeStaticLoad() {
     }
