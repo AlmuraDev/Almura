2d9d7c3aecf4df07f90386d875516a601fc00ee6
Mike Howe <mike@mcsnetworks.com>
2015-08-21 23:44:36 -0500

    BlockRenderer changes.
    1. Fixed lighting issue with custom lights.
    2. Fixed BlockFace rotations after implementing rp.deductParameters() and applyMatrix() method calls.
    3. Fixed Generic Cube not rotating after the above fix was implemented.
    
    Signed-off-by: Mike Howe <mike@mcsnetworks.com>

--- src/main/java/com/almuradev/almura/pack/renderer/BlockRenderer.java
+++ src/main/java/com/almuradev/almura/pack/renderer/BlockRenderer.java
@@ -5,6 +5,8 @@
  */
 package com.almuradev.almura.pack.renderer;
 
+import org.lwjgl.opengl.GL11;
+
 import com.almuradev.almura.Almura;
 import com.almuradev.almura.pack.IBlockClipContainer;
 import com.almuradev.almura.pack.IBlockModelContainer;
@@ -15,6 +17,7 @@ import com.almuradev.almura.pack.RotationMeta;
 import com.almuradev.almura.pack.model.IModel;
 import com.almuradev.almura.pack.model.PackFace;
 import com.almuradev.almura.pack.model.PackModelContainer;
+import com.almuradev.almura.pack.node.LightNode;
 import com.almuradev.almura.pack.node.RotationNode;
 import com.almuradev.almura.pack.node.property.RotationProperty;
 import com.google.common.base.Optional;
@@ -28,6 +31,7 @@ import net.malisis.core.renderer.element.Vertex;
 import net.malisis.core.renderer.element.shape.Cube;
 import net.malisis.core.renderer.icon.ClippedIcon;
 import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
 import net.minecraft.item.Item;
 import net.minecraft.util.IIcon;
 import net.minecraftforge.common.util.ForgeDirection;
@@ -108,8 +112,63 @@ public class BlockRenderer extends MalisisRenderer {
         }
         return super.getIcon(params);
     }
-    
-    
+
+    // This override is needed because we set our own light factor values and ignore Ambient Occlusion.
+    @Override
+    protected int calcVertexColor(Vertex vertex, int[][] aoMatrix)
+    {
+        int color = 0xFFFFFF;
+
+        if (params.usePerVertexColor.get()) //vertex should use their own colors
+            color = vertex.getColor();
+        if (params.colorMultiplier.get() != null) //global color multiplier is set
+            color = params.colorMultiplier.get();
+        else if (block != null) //use block color mulitplier
+            color = world != null ? block.colorMultiplier(world, x, y, z) : block.getRenderColor(blockMetadata);
+
+            if (drawMode == GL11.GL_LINE) //no AO for lines
+                return color;
+            if (renderType != RenderType.ISBRH_WORLD && renderType != RenderType.TESR_WORLD) //no AO for item/inventories
+                return color;
+
+            float factor = 1;
+
+            //calculate AO
+            if (params.calculateAOColor.get() && aoMatrix != null && Minecraft.isAmbientOcclusionEnabled()
+                    && block.getLightValue(world, x, y, z) == 0)
+            {
+                factor = getBlockAmbientOcclusion(world, x + params.direction.get().offsetX, y + params.direction.get().offsetY, z
+                        + params.direction.get().offsetZ);
+
+                for (int i = 0; i < aoMatrix.length; i++)
+                    factor += getBlockAmbientOcclusion(world, x + aoMatrix[i][0], y + aoMatrix[i][1], z + aoMatrix[i][2]);
+
+                factor /= (aoMatrix.length + 1);
+            }
+
+            //apply face dependent shading
+            factor *= params.colorFactor.get();
+
+            // Following additional check is to see if the block being rendered should respect Ambient Occlusions light factor or skip it.
+            // AlmuraMod's light type block have always appeared in bright white.
+            // Start AlmuraMod
+            if (block instanceof INodeContainer) {
+                LightNode node = ((INodeContainer) block).getNode(LightNode.class);
+                if (node != null && node.getEmission() > 0) {
+                    factor = 1;
+                }
+            }
+            // End AlmuraMod
+            int r = (int) ((color >> 16 & 255) * factor);
+            int g = (int) ((color >> 8 & 255) * factor);
+            int b = (int) ((color & 255) * factor);
+
+            color = r << 16 | g << 8 | b;
+
+            return color;
+    }
+
+    // The following method override is neede because Almura's original shape format was flawed. 
     @Override
     public void applyTexture(Shape shape, RenderParameters parameters)
     {
@@ -127,11 +186,20 @@ public class BlockRenderer extends MalisisRenderer {
                 boolean flipU = params.flipU.get();
                 boolean flipV = params.flipV.get();
                 if (params.direction.get() == ForgeDirection.NORTH || params.direction.get() == ForgeDirection.EAST) {
-                    //flipU = !flipU;  // Don't flip on this direction, we already do it.
+                    // Start AlmuraMod
+                    if (shape instanceof Cube) {  // Generic Shapes don't invert the NORTH / EAST face.
+                        flipU = !flipU;  // This method is skipped for shapes we've provided due to a flaw in how shapes were originally created for Spoutcraft.
+                    }
+                    // End AlmuraMod
                 }
 
                 f.setTexture(icon, flipU, params.flipV.get(), params.interpolateUV.get());
-                
+                if (block instanceof INodeContainer) {
+                    LightNode node = ((INodeContainer) block).getNode(LightNode.class);
+                    if (node != null && node.getEmission() > 0) {
+                        parameters.colorFactor.set(1f);
+                    }
+                }
             }
         }
     }
@@ -166,72 +234,72 @@ public class BlockRenderer extends MalisisRenderer {
         final RotationProperty property = node.getRotationProperty(rotation);
         if (property == null) {
             switch (rotation) {
-                case NORTH:
-                    if (node.isDefaultRotate()) {
-                        model.rotate(180f, 0, -1, 0);
-                    }
-                    break;
-                case SOUTH:
-                    break;
-                case WEST:
-                    if (node.isDefaultRotate()) {
-                        model.rotate(90f, 0, -1, 0);
-                    }
-                    break;
-                case EAST:
-                    if (node.isDefaultRotate()) {
-                        model.rotate(90f, 0, 1, 0);
-                    }
-                    break;
-                case DOWN_NORTH:
-                    if (node.isDefaultRotate()) {
-                        model.rotate(180f, 0, -1, 0);
-                    }
-                    if (node.isDefaultMirrorRotate()) {
-                        model.rotate(90f, -1, 0, 0);
-                    }
-                    break;
-                case DOWN_SOUTH:
-                    break;
-                case DOWN_WEST:
-                    if (node.isDefaultRotate()) {
-                        model.rotate(90f, 0, -1, 0);
-                    }
-                    if (node.isDefaultMirrorRotate()) {
-                        model.rotate(180f, -1, 0, 0);
-                    }
-                    break;
-                case DOWN_EAST:
-                    if (node.isDefaultRotate()) {
-                        model.rotate(90f, 0, 1, 0);
-                    }
-                    if (node.isDefaultMirrorRotate()) {
-                        model.rotate(180f, -1, 0, 0);
-                    }
-                    break;
-                case UP_NORTH:
-                    if (node.isDefaultRotate()) {
-                        model.rotate(180f, 0, -1, 0);
-                    }
-                    break;
-                case UP_SOUTH:
-                    break;
-                case UP_WEST:
-                    if (node.isDefaultRotate()) {
-                        model.rotate(90f, 0, -1, 0);
-                    }
-                    break;
-                case UP_EAST:
-                    if (node.isDefaultRotate()) {
-                        model.rotate(90f, 0, 1, 0);
-                    }
-                    break;
+            case NORTH:
+                if (node.isDefaultRotate()) {
+                    model.rotate(180f, 0, -1, 0);
+                }
+                break;
+            case SOUTH:
+                break;
+            case WEST:
+                if (node.isDefaultRotate()) {
+                    model.rotate(90f, 0, -1, 0);
+                }
+                break;
+            case EAST:
+                if (node.isDefaultRotate()) {
+                    model.rotate(90f, 0, 1, 0);
+                }
+                break;
+            case DOWN_NORTH:
+                if (node.isDefaultRotate()) {
+                    model.rotate(180f, 0, -1, 0);
+                }
+                if (node.isDefaultMirrorRotate()) {
+                    model.rotate(90f, -1, 0, 0);
+                }
+                break;
+            case DOWN_SOUTH:
+                break;
+            case DOWN_WEST:
+                if (node.isDefaultRotate()) {
+                    model.rotate(90f, 0, -1, 0);
+                }
+                if (node.isDefaultMirrorRotate()) {
+                    model.rotate(180f, -1, 0, 0);
+                }
+                break;
+            case DOWN_EAST:
+                if (node.isDefaultRotate()) {
+                    model.rotate(90f, 0, 1, 0);
+                }
+                if (node.isDefaultMirrorRotate()) {
+                    model.rotate(180f, -1, 0, 0);
+                }
+                break;
+            case UP_NORTH:
+                if (node.isDefaultRotate()) {
+                    model.rotate(180f, 0, -1, 0);
+                }
+                break;
+            case UP_SOUTH:
+                break;
+            case UP_WEST:
+                if (node.isDefaultRotate()) {
+                    model.rotate(90f, 0, -1, 0);
+                }
+                break;
+            case UP_EAST:
+                if (node.isDefaultRotate()) {
+                    model.rotate(90f, 0, 1, 0);
+                }
+                break;
             }
         } else {
             model.rotate(property.getAngle(), property.getX().getId(), property.getY().getId(), property.getZ().getId());
         }
-         ((Shape) model).applyMatrix();
-         ((Shape) model).deductParameters();
+        ((Shape) model).applyMatrix();
+        ((Shape) model).deductParameters();
     }
 
     private void handleScaling(IModel model) {
