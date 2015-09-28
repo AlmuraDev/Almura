fcc4ee78c320577a4b824fcfb4bcfee54ca1a279
Mike Howe - Dockter <mike@mcsnetworks.com>
2015-09-27 21:57:07 -0500

    Allow the backpack key to close the inventory window.
    Signed-off-by: Mike Howe - Dockter <mike@mcsnetworks.com>

---
+++ src/main/java/com/almuradev/almura/mixin/client/gui/inventory/MixinGuiContainer.java
@@ -0,0 +1,27 @@
+/**
+ * This file is part of Almura, All Rights Reserved.
+ *
+ * Copyright (c) 2014 AlmuraDev <http://github.com/AlmuraDev/>
+ */
+package com.almuradev.almura.mixin.client.gui.inventory;
+
+import net.minecraft.client.gui.inventory.GuiContainer;
+
+import com.almuradev.almura.client.Bindings;
+import net.minecraft.client.gui.GuiScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(GuiContainer.class)
+public abstract class MixinGuiContainer extends GuiScreen { 
+
+    @Inject(method = "keyTyped", at = @At("HEAD"))
+    public void keyTyped(char p_73869_1_, int p_73869_2_, CallbackInfo ci) {
+        if (p_73869_2_ == 1 || p_73869_2_ == Bindings.BINDING_OPEN_BACKPACK.getKeyCode())
+        {
+            this.mc.thePlayer.closeScreen();
+        }       
+    }   
+}
