fefc56674c3bd08166cd9cba619ee14deb7ebee5
Chris Sanders <zidane@outlook.com>
2015-10-05 23:42:56 -0500

    Correct license header to read in current year.
    Signed-off-by: Chris Sanders <zidane@outlook.com>

--- src/main/java/com/almuradev/almura/pack/crop/PackCrops.java
+++ src/main/java/com/almuradev/almura/pack/crop/PackCrops.java
@@ -1,7 +1,7 @@
 /**
  * This file is part of Almura, All Rights Reserved.
  *
- * Copyright (c) 2014 AlmuraDev <http://github.com/AlmuraDev/>
+ * Copyright (c) 2014 - 2015 AlmuraDev <http://github.com/AlmuraDev/>
  */
 package com.almuradev.almura.pack.crop;
 
