ef05b7d04409f760d99c56163efd4c0b4ef02b78
Steven Downer <grinch@outlook.com>
2016-08-12 00:11:22 -0500

    Update license header
    Signed-off-by: Steven Downer <grinch@outlook.com>

--- src/main/java/com/almuradev/almura/mixin/core/client/gui/MixinGuiMultiplayer.java
+++ src/main/java/com/almuradev/almura/mixin/core/client/gui/MixinGuiMultiplayer.java
@@ -1,3 +1,8 @@
+/**
+ * This file is part of Almura, All Rights Reserved.
+ *
+ * Copyright (c) AlmuraDev <http://github.com/AlmuraDev/>
+ */
 package com.almuradev.almura.mixin.core.client.gui;
 
 import net.minecraft.client.gui.GuiButton;
