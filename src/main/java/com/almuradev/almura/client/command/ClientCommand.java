409e132cf9d04d598096cf8eb3dbc66ab4a01fd4
Mike Howe - Dockter <mike@mcsnetworks.com>
2016-11-22 20:09:38 -0600

    B129 changes. - Implement Salt Ore.
    Signed-off-by: Mike Howe - Dockter <mike@mcsnetworks.com>

--- src/main/java/com/almuradev/almura/client/command/ClientCommand.java
+++ src/main/java/com/almuradev/almura/client/command/ClientCommand.java
@@ -79,7 +79,7 @@ public class ClientCommand extends CommandBase {
                 break;
                 
             case "TRY":
-                
+
                 break;
                 
             case "REFRESHSKIN":
--- src/main/java/com/almuradev/almura/pack/generator/PackGenerator.java
+++ src/main/java/com/almuradev/almura/pack/generator/PackGenerator.java
@@ -18,24 +18,36 @@ import net.minecraft.world.gen.feature.WorldGenMinable;
 import java.util.Random;
 
 public class PackGenerator implements IWorldGenerator {
-    private final Block replacement;
+    private final Block marbleOre;
+    private final Block saltOre;
 
     public PackGenerator() {
-        this.replacement = GameRegistry.findBlock(Almura.MOD_ID, "Ores\\marbleOre");
+        this.marbleOre = GameRegistry.findBlock(Almura.MOD_ID, "Ores\\marbleOre");
+        this.saltOre = GameRegistry.findBlock(Almura.MOD_ID, "Ores\\saltOre");
     }
 
     @Override
     public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
         if (!world.isRemote && world.provider instanceof WorldProviderSurface) {
             // Replacement | Meta | Ore Vein Size | Target
-            final WorldGenMinable genMinable = new WorldGenMinable(this.replacement, 0, 5, Blocks.stone);
+            final WorldGenMinable genMarble = new WorldGenMinable(this.marbleOre, 0, 5, Blocks.stone);
             for (int numVeins = 0; numVeins < 3; numVeins++) {
                 final int blockX =  (chunkX * 16) + random.nextInt(16);
                 final int blockY = random.nextInt(64);
                 final int blockZ = (chunkZ * 16) + random.nextInt(16);
 
                 //System.out.println("Calling Generate of Marble Ore. X [" + blockX + "], Y [" + blockY + "], Z [" + blockZ + "].");
-                genMinable.generate(world, random, blockX, blockY, blockZ);
+                genMarble.generate(world, random, blockX, blockY, blockZ);
+            }
+            
+            final WorldGenMinable genSalt = new WorldGenMinable(this.saltOre, 0, 10, Blocks.stone);
+            for (int numVeins = 0; numVeins < 3; numVeins++) {
+                final int blockX =  (chunkX * 16) + random.nextInt(16);
+                final int blockY = random.nextInt(64);
+                final int blockZ = (chunkZ * 16) + random.nextInt(16);
+
+                //System.out.println("Calling Generate of Marble Ore. X [" + blockX + "], Y [" + blockY + "], Z [" + blockZ + "].");
+                genSalt.generate(world, random, blockX, blockY, blockZ);
             }
         }
     }
