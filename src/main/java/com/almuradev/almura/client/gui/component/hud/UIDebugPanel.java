b7a824a64fbe73acffbc3f7e8838cbf3839d4e96
kashike <kashike@vq.lc>
2017-06-25 23:43:47 -0700

    Some changes to the HUD debug panel

--- src/main/java/com/almuradev/almura/client/gui/component/hud/UIDebugPanel.java
+++ src/main/java/com/almuradev/almura/client/gui/component/hud/UIDebugPanel.java
@@ -15,10 +15,12 @@ import net.minecraft.block.Block;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
-import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.Entity;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.EnumSkyBlock;
 import net.minecraft.world.WorldType;
@@ -29,7 +31,6 @@ import org.spongepowered.api.text.format.TextColors;
 import org.spongepowered.api.text.serializer.TextSerializers;
 
 import java.util.Map;
-import java.util.Optional;
 
 
 public class UIDebugPanel extends UIHUDPanel {
@@ -37,7 +38,9 @@ public class UIDebugPanel extends UIHUDPanel {
     private static final TextTemplate KEY_VALUE_TEXT_TEMPLATE = TextTemplate.of(
             TextTemplate.arg("key").color(TextColors.WHITE), ": ",
             TextTemplate.arg("value").color(TextColors.GRAY));
-    private Minecraft minecraft = Minecraft.getMinecraft();
+    private final Minecraft minecraft = Minecraft.getMinecraft();
+    private final Text title;
+    private final int titleWidth;
     private MalisisFont font = MalisisFont.minecraftFont;
     private FontOptions fontOptions = FontOptionsConstants.FRO_COLOR_WHITE;
     private boolean autoSize = true;
@@ -50,11 +53,16 @@ public class UIDebugPanel extends UIHUDPanel {
         super(gui, width, height);
         this.baseWidth = width;
         this.baseHeight = height;
+
+        final String title = "Minecraft " + this.minecraft.getVersion();
+        this.title = Text.of(TextColors.GOLD, title);
+        this.titleWidth = this.minecraft.fontRenderer.getStringWidth(title) / 2;
     }
 
     @Override
     public void drawForeground(GuiRenderer renderer, int mouseX, int mouseY, float partialTick) {
-        if (this.minecraft.player == null || this.minecraft.player.world == null || !this.minecraft.gameSettings.showDebugInfo) {
+        final Entity view = this.minecraft.getRenderViewEntity();
+        if (view == null || this.minecraft.player == null || this.minecraft.player.world == null || !this.minecraft.gameSettings.showDebugInfo) {
             return;
         }
 
@@ -62,64 +70,93 @@ public class UIDebugPanel extends UIHUDPanel {
 
         this.minecraft.mcProfiler.startSection("debug");
 
-        // Properties
-        final EntityLivingBase viewer = Optional.ofNullable((EntityLivingBase) this.minecraft.getRenderViewEntity()).orElse(this.minecraft.player);
-        final BlockPos viewerPos = new BlockPos(viewer.posX, viewer.posY, viewer.posZ);
-        final Chunk chunk = viewer.getEntityWorld().getChunkFromBlockCoords(viewerPos);
-
-        // Title
-        final String title = "Minecraft " + this.minecraft.getVersion();
-        final int titleWidth = this.minecraft.fontRenderer.getStringWidth(title);
-        this.drawText(renderer, Text.of(TextColors.GOLD, title), (Math.max(this.autoWidth, this.baseWidth) / 2) - (titleWidth / 2) - 2, 4);
-
+        this.drawText(renderer, this.title, (Math.max(this.autoWidth, this.baseWidth) / 2) - this.titleWidth - 2, 4);
         // Reset autoWidth after we've drawn the title
         if (this.autoSize) {
             this.autoWidth = 0;
         }
 
-        // Keys/Values
+        // Position
+        final double x = view.posX;
+        final int fx = MathHelper.floor(x);
+        final double y = view.getEntityBoundingBox().minY; // vanilla uses minY of the AABB
+        final int fy = MathHelper.floor(y);
+        final double z = view.posZ;
+        final int fz = MathHelper.floor(z);
+        final BlockPos pos = new BlockPos(x, y, z);
+        final Chunk chunk = view.getEntityWorld().getChunkFromBlockCoords(pos);
+
+        // Standard details
         this.drawProperty(renderer, "Java", this.getJavaDetails(), 4, this.autoHeight += 4);
         this.drawProperty(renderer, "Memory", this.getMemoryDetails(), 4, this.getAutoSizeHeight());
         this.drawProperty(renderer, "FPS", String.valueOf(Minecraft.getDebugFPS()), 4, this.getAutoSizeHeight());
-        this.drawProperty(renderer, "X", String.format("%.5f", this.minecraft.player.posX), 4, this.getAutoSizeHeight());
-        this.drawProperty(renderer, "Y", String.format("%.5f", this.minecraft.player.posY), 4, this.getAutoSizeHeight());
-        this.drawProperty(renderer, "Z", String.format("%.5f", this.minecraft.player.posZ), 4, this.getAutoSizeHeight());
-        this.drawProperty(renderer, "Block", this.getBlockDetails(viewerPos), 4, this.getAutoSizeHeight());
-        this.drawProperty(renderer, "Chunk", this.getChunkDetails(viewerPos), 4, this.getAutoSizeHeight());
-        if (viewer.getEntityWorld().isBlockLoaded(viewerPos) && viewerPos.getY() >= 0 && viewerPos.getY() < 256 && !chunk.isEmpty()) {
-            this.drawProperty(renderer, "Biome", chunk.getBiome(viewerPos, this.minecraft.world.getBiomeProvider()).getBiomeName(),
-                    4, this.getAutoSizeHeight());
-            this.drawProperty(renderer, "Light", this.getLightDetails(viewerPos, chunk),4, this.getAutoSizeHeight());
+        // Respect reduced debug
+        if (this.minecraft.isReducedDebug()) {
+            this.drawProperty(renderer, "Chunk-relative", String.format("%d %d %d", fx & 15, fy & 15, fz & 15), 4, this.getAutoSizeHeight());
+        } else {
+            this.drawProperty(renderer, "XYZ", String.format("%.3f, %.3f, %.3f", view.posX, y, view.posZ), 4, this.getAutoSizeHeight());
+            final EnumFacing facing = view.getHorizontalFacing();
+            final String facingTowards;
+            switch (facing) {
+                case NORTH:
+                    facingTowards = " (-z)";
+                    break;
+                case SOUTH:
+                    facingTowards = " (+z)";
+                    break;
+                case WEST:
+                    facingTowards = " (-x)";
+                    break;
+                case EAST:
+                    facingTowards = " (+x)";
+                    break;
+                default: // invalid
+                    facingTowards = "";
+            }
+            this.drawProperty(renderer, "Facing", String.format("%s%s (%.1f, %.1f)", facing.name().charAt(0), facingTowards,
+                    MathHelper.wrapDegrees(view.rotationYaw), MathHelper.wrapDegrees(view.rotationPitch)), 4, this.getAutoSizeHeight());
+            this.drawProperty(renderer, "Block", fx + ", " + fy + ", " + fz, 4, this.getAutoSizeHeight());
+            this.drawProperty(renderer, "Chunk", String.format("%d %d %d in %d %d %d", fx & 15, fy & 15, fz & 15, fx >> 4, fy >> 4, fz >> 4), 4, this.getAutoSizeHeight());
+            if (view.getEntityWorld().isBlockLoaded(pos) && pos.getY() >= 0 && pos.getY() < 256 && !chunk.isEmpty()) {
+                this.drawProperty(renderer, "Biome", chunk.getBiome(pos, this.minecraft.world.getBiomeProvider()).getBiomeName(),
+                        4, this.getAutoSizeHeight());
+                this.drawProperty(renderer, "Light", this.getLightDetails(pos, chunk),4, this.getAutoSizeHeight());
+            }
         }
 
         // Draw block we're currently looking at
-        if (this.minecraft.objectMouseOver != null && this.minecraft.objectMouseOver.typeOfHit == RayTraceResult.Type.BLOCK) {
-            final BlockPos blockPos = this.minecraft.objectMouseOver.getBlockPos();
-            IBlockState blockState = this.minecraft.world.getBlockState(blockPos);
+        final RayTraceResult omo = this.minecraft.objectMouseOver;
+        if (omo != null && omo.typeOfHit == RayTraceResult.Type.BLOCK && omo.getBlockPos() != null) {
+            final BlockPos lookPos = omo.getBlockPos();
 
+            this.drawProperty(renderer, "Look", String.format("%d %d %d", lookPos.getX(), lookPos.getY(), lookPos.getZ()), 4, this.getAutoSizeHeight());
+
+            IBlockState lookState = this.minecraft.world.getBlockState(lookPos);
             if (this.minecraft.world.getWorldType() != WorldType.DEBUG_ALL_BLOCK_STATES) {
-                blockState = blockState.getActualState(this.minecraft.world, blockPos);
+                lookState = lookState.getActualState(this.minecraft.world, lookPos);
             }
 
             this.clipContent = false;
 
-            if (blockState.getBlock() != Blocks.AIR) {
-                this.drawBlock(renderer, blockState, 4, this.getAutoSizeHeight() + 2);
-                this.drawText(renderer, Text.of(TextColors.WHITE, Block.REGISTRY.getNameForObject(blockState.getBlock())), 24,
-                        this.autoHeight - 18, false, true);
+            if (lookState.getBlock() != Blocks.AIR) {
+                this.drawBlock(renderer, lookState, 4, this.getAutoSizeHeight() + 2);
+                this.drawText(renderer, Text.of(TextColors.WHITE, Block.REGISTRY.getNameForObject(lookState.getBlock())), 24, this.autoHeight - 18, false, true);
 
-                boolean hasValidContent = blockState.getProperties().entrySet().stream().anyMatch(e -> e.getValue() instanceof Boolean);
+                final boolean hasValidContent = !lookState.getProperties().isEmpty();
 
                 if (hasValidContent) {
                     this.autoHeight -= 6;
                 }
 
-                for (Map.Entry<IProperty<?>, Comparable<?>> entry : blockState.getProperties().entrySet()) {
-                    if (entry.getValue() instanceof Boolean) {
-                        this.drawText(renderer,
-                                Text.of(TextColors.WHITE, entry.getKey().getName(), ": ",
-                                        ((Boolean) entry.getValue() ? TextColors.GREEN : TextColors.RED), entry.getValue()),24, this.autoHeight);
-
+                for (Map.Entry<IProperty<?>, Comparable<?>> entry : lookState.getProperties().entrySet()) {
+                    final IProperty property = entry.getKey();
+                    final String name = property.getName();
+                    final Comparable value = entry.getValue();
+                    final String describedValue = property.getName(value);
+                    if (value instanceof Boolean) {
+                        this.drawText(renderer, Text.of(TextColors.WHITE, name, ": ", ((Boolean) value) ? TextColors.GREEN : TextColors.RED, describedValue),24, this.autoHeight);
+                    } else {
+                        this.drawProperty(renderer, name, describedValue, 24, this.autoHeight);
                     }
                 }
 
@@ -128,6 +165,7 @@ public class UIDebugPanel extends UIHUDPanel {
                 }
             }
         }
+
         this.autoHeight += 4; // Extra padding
 
         // AutoSizing
@@ -199,7 +237,7 @@ public class UIDebugPanel extends UIHUDPanel {
     }
 
     private String getJavaDetails() {
-        return System.getProperty("java.version") + " " + (this.minecraft.isJava64bit() ? 64 : 32) + "-bit";
+        return System.getProperty("java.version") + " " + (this.minecraft.isJava64bit() ? 64 : 32) + "bit";
     }
 
     private String getMemoryDetails() {
@@ -208,25 +246,6 @@ public class UIDebugPanel extends UIHUDPanel {
         final long freeMemory = Runtime.getRuntime().freeMemory();
         final long usedMemory = totalMemory - freeMemory;
         return (usedMemory * 100L / maxMemory) + "%% " + convertBytesToMegabytes(usedMemory) + "/" + convertBytesToMegabytes(maxMemory) + "MB";
-    }
-
-    private String getAllocatedDetails() {
-        final long maxMemory = Runtime.getRuntime().maxMemory();
-        final long totalMemory = Runtime.getRuntime().totalMemory();
-        return (totalMemory * 100L / maxMemory) + "%% " + convertBytesToMegabytes(totalMemory) + "MB";
-    }
-
-    private String getBlockDetails(BlockPos viewerPos) {
-        return viewerPos.getX() + ", " + viewerPos.getY() + ", " + viewerPos.getZ();
-    }
-
-    private String getChunkDetails(BlockPos viewerPos) {
-        return (viewerPos.getX() % 15) + " " +
-                (viewerPos.getY() % 15) + " " +
-                (viewerPos.getZ() % 15) + " in " +
-                (viewerPos.getX() >> 4) + " " +
-                (viewerPos.getY() >> 4) + " " +
-                (viewerPos.getZ() >> 4);
     }
 
     private String getLightDetails(BlockPos viewerPos, Chunk chunk) {
