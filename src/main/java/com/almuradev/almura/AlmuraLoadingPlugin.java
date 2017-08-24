067c98fb3cf3652e63e76847dad9f18eb0ec0b41
kashike <kashike@vq.lc>
2017-08-23 20:14:56 -0700

    build: SpongeForge to 1.12.1-2444-7.0.0-BETA-2601

--- src/main/java/com/almuradev/almura/AlmuraLoadingPlugin.java
+++ src/main/java/com/almuradev/almura/AlmuraLoadingPlugin.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.Mixins;
 
 import java.util.Map;
 
-@IFMLLoadingPlugin.MCVersion("1.12")
+@IFMLLoadingPlugin.MCVersion("1.12.1")
 public final class AlmuraLoadingPlugin implements IFMLLoadingPlugin {
 
     public AlmuraLoadingPlugin() {
