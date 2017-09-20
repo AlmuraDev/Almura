45b39734b3dac1a641ec378c7169b92f052bdc8d
Mike Howe - Dockter <mike@mcsnetworks.com>
2017-09-19 20:29:09 -0500

    Created Mixin's to silence servers notifications for Missing Entities and TileEntities.
    Signed-off-by: Mike Howe - Dockter <mike@mcsnetworks.com>

---
+++ src/main/java/com/almuradev/almura/asm/mixin/core/entity/MixinEntityList.java
@@ -0,0 +1,20 @@
+/*
+ * This file is part of Almura, All Rights Reserved.
+ *
+ * Copyright (c) AlmuraDev <http://github.com/AlmuraDev/>
+ */
+package com.almuradev.almura.asm.mixin.core.entity;
+
+import net.minecraft.entity.EntityList;
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(EntityList.class)
+public class MixinEntityList {
+    @Redirect(method = "createEntityFromNBT", at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;)V"))
+    private static void silenceNonExistantEntity(Logger logger, String message, Object p0) {
+        //This Mixin is here to silence the feedback o.
+    }
+}
---
+++ src/main/java/com/almuradev/almura/asm/mixin/core/tileentity/MixinTileEntity.java
@@ -0,0 +1,20 @@
+/*
+ * This file is part of Almura, All Rights Reserved.
+ *
+ * Copyright (c) AlmuraDev <http://github.com/AlmuraDev/>
+ */
+package com.almuradev.almura.asm.mixin.core.tileentity;
+
+import net.minecraft.tileentity.TileEntity;
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(TileEntity.class)
+public class MixinTileEntity {
+    @Redirect(method = "create", at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;)V"))
+    private static void silenceTileEntityNotFound(Logger logger, String message, Object p0) {
+        // Silence server console spam.
+    }
+}
