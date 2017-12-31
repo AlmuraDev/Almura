e25cb8e79e3091404ce157a55cdc1f644f1bf9e2
kashike <kashike@vq.lc>
2017-12-31 09:05:43 -0800

    no idea if this will work

--- src/main/java/com/almuradev/almura/feature/biome/ReadOnlyBiomeModule.java
+++ src/main/java/com/almuradev/almura/feature/biome/ReadOnlyBiomeModule.java
@@ -7,22 +7,25 @@
  */
 package com.almuradev.almura.feature.biome;
 
+import com.almuradev.almura.shared.inject.CommonBinder;
 import com.google.inject.Injector;
 import com.google.inject.Provides;
 import net.kyori.violet.AbstractModule;
 import net.minecraftforge.fml.common.Loader;
+import org.spongepowered.api.Platform;
 
 import javax.inject.Singleton;
 
-public final class ReadOnlyBiomeModule extends AbstractModule {
+public final class ReadOnlyBiomeModule extends AbstractModule implements CommonBinder {
     @Override
     protected void configure() {
+        this.on(Platform.Type.CLIENT, () -> this.facet().add(TerrainControlReadOnlyBiomeSource.ClientNotifier.class));
     }
 
     @Provides
     @Singleton
     ReadOnlyBiomeSource readOnlyBiomeSource(final Injector injector) {
-        if (Loader.isModLoaded("terraincontrol")) {
+        if (Loader.isModLoaded(TerrainControlReadOnlyBiomeSource.TERRAIN_CONTROL_ID)) {
             return injector.getInstance(TerrainControlReadOnlyBiomeSource.class);
         }
         return new VanillaReadOnlyBiomeSource();
--- src/main/java/com/almuradev/almura/feature/biome/TerrainControlReadOnlyBiomeSource.java
+++ src/main/java/com/almuradev/almura/feature/biome/TerrainControlReadOnlyBiomeSource.java
@@ -8,18 +8,37 @@
 package com.almuradev.almura.feature.biome;
 
 import com.almuradev.almura.feature.hud.HeadUpDisplay;
+import com.almuradev.almura.shared.event.Witness;
 import com.google.common.base.MoreObjects;
 import com.khorn.terraincontrol.LocalBiome;
 import com.khorn.terraincontrol.LocalWorld;
 import com.khorn.terraincontrol.TerrainControl;
+import com.khorn.terraincontrol.configuration.ConfigProvider;
+import com.khorn.terraincontrol.configuration.ConfigToNetworkSender;
+import com.khorn.terraincontrol.configuration.standard.PluginStandardValues;
 import com.khorn.terraincontrol.forge.util.WorldHelper;
+import com.khorn.terraincontrol.logging.LogMarker;
+import io.netty.buffer.ByteBufOutputStream;
+import io.netty.buffer.Unpooled;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.network.play.server.SPacketCustomPayload;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.Loader;
+import org.spongepowered.api.GameState;
+import org.spongepowered.api.entity.Entity;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.entity.MoveEntityEvent;
+
+import java.io.DataOutput;
+import java.io.IOException;
 
 import javax.annotation.Nullable;
 import javax.inject.Inject;
 
 public final class TerrainControlReadOnlyBiomeSource extends ReadOnlyBiomeSource {
+    static final String TERRAIN_CONTROL_ID = "terraincontrol";
     private final HeadUpDisplay hud;
 
     @Inject
@@ -45,5 +64,41 @@ public final class TerrainControlReadOnlyBiomeSource extends ReadOnlyBiomeSource
             };
         }
         return super.getBiome(world, pos);
+    }
+
+    public static class ClientNotifier implements Witness.Lifecycle {
+        @Override
+        public boolean lifecycleSubscribable(final GameState state) {
+            return state == GameState.SERVER_STARTING && Loader.isModLoaded(TERRAIN_CONTROL_ID);
+        }
+
+        @Listener
+        public void worldChange(final MoveEntityEvent.Teleport event) {
+            final Entity entity = event.getTargetEntity();
+            if (!(entity instanceof EntityPlayerMP)) {
+                return;
+            }
+
+            final EntityPlayerMP player = (EntityPlayerMP) entity;
+
+            final LocalWorld world = WorldHelper.toLocalWorld(player.getEntityWorld());
+            if (world == null) {
+                return;
+            }
+
+            final ConfigProvider config = world.getConfigs();
+
+            final PacketBuffer buf = new PacketBuffer(Unpooled.buffer());
+            final DataOutput os = new ByteBufOutputStream(buf);
+            try {
+                os.writeInt(PluginStandardValues.ProtocolVersion);
+                ConfigToNetworkSender.send(config, os);
+            } catch (final IOException e) {
+                TerrainControl.printStackTrace(LogMarker.FATAL, e);
+            }
+
+            final SPacketCustomPayload packet = new SPacketCustomPayload(PluginStandardValues.ChannelName, buf);
+            player.connection.sendPacket(packet);
+        }
     }
 }
