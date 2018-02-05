7f6eeb3b52abfa049cb2b4c5412d5defdaee5c46
kashike <kashike@vq.lc>
2018-02-05 14:56:03 -0800

    enable shovels

--- src/main/java/com/almuradev/content/type/item/type/tool/ToolItemModule.java
+++ src/main/java/com/almuradev/content/type/item/type/tool/ToolItemModule.java
@@ -10,11 +10,13 @@ package com.almuradev.content.type.item.type.tool;
 import com.almuradev.content.type.item.ItemModule;
 import com.almuradev.content.type.item.type.tool.type.hoe.HoeToolItemModule;
 import com.almuradev.content.type.item.type.tool.type.pickaxe.PickaxeToolItemModule;
+import com.almuradev.content.type.item.type.tool.type.shovel.ShovelToolItemModule;
 
 public final class ToolItemModule extends ItemModule.Module {
     @Override
     protected void configure() {
         this.install(new HoeToolItemModule());
         this.install(new PickaxeToolItemModule());
+        this.install(new ShovelToolItemModule());
     }
 }
