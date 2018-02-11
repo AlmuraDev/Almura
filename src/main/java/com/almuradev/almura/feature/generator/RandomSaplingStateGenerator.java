d8952447aed990b0b2dc1f1407582dd7c37ef95f
Dockter <dockter@almuramc.com>
2018-02-11 15:16:00 -0600

    Prepare for Saplings that @Kashike is working on.  Planning on using the flower generator to randomly place saplings in the world instead of using a tree generator.
    Signed-off-by: Dockter <dockter@almuramc.com>

---
+++ src/main/java/com/almuradev/almura/feature/generator/RandomSaplingStateGenerator.java
@@ -0,0 +1,53 @@
+package com.almuradev.almura.feature.generator;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import java.util.Random;
+
+public class RandomSaplingStateGenerator {
+    static boolean debug = false;
+
+    public static IBlockState randomSapling(World worldIn, BlockPos blockPos, Random random) {
+        String saplingName = "";
+        int saplingAge = 0;
+        int sapling = random.nextInt(64); // Total Crops
+
+        if (sapling == 0) {
+            saplingName = "almura:sapling/cheery_sapling";
+        } else if (sapling == 1) {
+            saplingName = "almura:sapling/grape_fruit_sapling";
+        } else if (sapling == 2) {
+            saplingName = "almura:sapling/apple_sapling";
+        } else if (sapling == 3) {
+            saplingName = "almura:sapling/kiwi_sapling";
+        } else if (sapling == 4) {
+            saplingName = "almura:sapling/lemon_sapling";
+        } else if (sapling == 5) {
+            saplingName = "almura:sapling/lime_sapling";
+        } else if (sapling == 6) {
+            saplingName = "almura:sapling/orange_sapling";
+        } else if (sapling == 7) {
+            saplingName = "almura:sapling/peach_sapling";
+        } else if (sapling == 8) {
+            saplingName = "almura:sapling/plum_sapling";
+        } else if (sapling == 9) {
+            saplingName = "almura:sapling/walnut_sapling";
+        } else if (sapling == 10) {
+            saplingName = "almura:sapling/green_apple_sapling";
+        }
+        
+        if (debug) {
+            System.out.println("RandomSaplingStateGenerator - Sapling: " + saplingName + " at: " + blockPos);
+        }
+
+        final Block block = Block.getBlockFromName(saplingName);
+        if (block != null) {
+            final IBlockState state = block.getStateFromMeta(saplingAge);
+            return state;
+        }
+        return null;
+    }
+}
