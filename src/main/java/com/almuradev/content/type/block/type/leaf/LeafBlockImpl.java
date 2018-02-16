64fbfb13a9f7660911529ed1407bd082adbe6a11
kashike <kashike@vq.lc>
2018-02-15 16:54:23 -0800

    EVERYTHING IS STUPID

--- src/main/java/com/almuradev/content/type/block/type/leaf/LeafBlockImpl.java
+++ src/main/java/com/almuradev/content/type/block/type/leaf/LeafBlockImpl.java
@@ -10,8 +10,10 @@ package com.almuradev.content.type.block.type.leaf;
 import com.almuradev.content.type.block.type.leaf.state.LeafBlockStateDefinition;
 import net.minecraft.block.BlockLeaves;
 import net.minecraft.block.BlockPlanks;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 
@@ -29,17 +31,22 @@ public final class LeafBlockImpl extends BlockLeaves implements LeafBlock {
     }
 
     @Override
+    protected BlockStateContainer createBlockState() {
+        return new BlockStateContainer(this, CHECK_DECAY, DECAYABLE);
+    }
+
+    @Override
     public LeafBlockStateDefinition definition(final IBlockState state) {
         return this.definition;
     }
 
     @Override
     public BlockPlanks.EnumType getWoodType(final int meta) {
-        return null; // TODO
+        return BlockPlanks.EnumType.DARK_OAK; // I'm not even related to dark_oak, but this goes away with 1.13
     }
 
     @Override
     public List<ItemStack> onSheared(@Nonnull final ItemStack item, final IBlockAccess world, final BlockPos pos, final int fortune) {
-        return null; // TODO
+        return NonNullList.withSize(1, new ItemStack(this));
     }
 }
