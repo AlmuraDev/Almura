2268332c4509216d79d6a2b6a704a3e2fe04097c
Chris Sanders <zidane@spongepowered.org>
2018-02-19 21:14:29 -0600

    Always forget licenses...
    Signed-off-by: Chris Sanders <zidane@spongepowered.org>

--- src/main/java/com/almuradev/content/model/ModelUtil.java
+++ src/main/java/com/almuradev/content/model/ModelUtil.java
@@ -1,3 +1,10 @@
+/*
+ * This file is part of Almura.
+ *
+ * Copyright (c) AlmuraDev <https://github.com/AlmuraDev/>
+ *
+ * All Rights Reserved.
+ */
 package com.almuradev.content.model;
 
 import com.almuradev.content.model.obj.OBJBakedModel;
