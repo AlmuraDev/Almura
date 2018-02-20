dccab05a47fc87d48c17422d19b764869b4fe0bf
Dockter <dockter@almuramc.com>
2018-02-20 12:08:37 -0600

    Define additional properties of an MTL.
    Signed-off-by: Dockter <dockter@almuramc.com>

--- src/main/java/com/almuradev/content/model/obj/OBJModelConfig.java
+++ src/main/java/com/almuradev/content/model/obj/OBJModelConfig.java
@@ -26,6 +26,20 @@ public interface OBJModelConfig {
     String VERTEX_NORMAL = "vn";
     String VERTEX_TEXTURE_COORDINATE = "vt";
 
+    // Reference Materials:  http://paulbourke.net/dataformats/mtl/  && http://people.sc.fsu.edu/~jburkardt/data/mtl/mtl.html
+
+    // Additional features of a MTL file from 3ds
+    String SpecularExponent = "Ns";       // Specifies the specular exponent for the current material.  This defines the focus of the specular highlight.
+    String OpticalDensity = "Ni";         // Specifies the optical density for the surface.
+    String Factor = "d";                  //"factor" is the amount this material dissolves into the background.  A factor of 0.0 is fully dissolved (completely transparent).
+    String TransparencyAlpha = "Tr";      // Transparency value set in Alfa.
+    String TransmissionFilter = "Tf";     // The Tf statement specifies the transmission filter using RGB values.
+    String IlluminiationModel = "illum";  // The "illum" statement specifies the illumination model to use in the material.
+    String AmbientReflectivity = "Ka";    // The Ka statement specifies the ambient reflectivity using RGB values.
+    String SpecularReflectivity = "Ks";   // The Ks statement specifies the specular reflectivity using RGB values.
+    String DiffuseReflectivity = "Kd";    // The Kd statement specifies the diffuse reflectivity using RGB values.
+    String UnknownValue = "Ke";           // Unknown Value.
+
     interface Material {
 
         String DIFFUSE = "map_Kd";
