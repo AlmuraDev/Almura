c2fa276f18d565b43ec99b08a419907e7edd18bb
Wifee <wifee@almuramc.com>
2018-02-21 22:54:01 -0600

    Removed Quantum Chest and added three others.
    Signed-off-by: Wifee <wifee@almuramc.com>

--- src/main/java/com/almuradev/almura/feature/storage/block/StorageBlocks.java
+++ src/main/java/com/almuradev/almura/feature/storage/block/StorageBlocks.java
@@ -12,5 +12,7 @@ import net.minecraft.util.ResourceLocation;
 
 public final class StorageBlocks {
     public static final StorageBlock DOCK_CHEST = new StorageBlock(new ResourceLocation(Almura.ID, "storage/dock_chest"), 54);
-    public static final StorageBlock QUANTUM_CHEST = new StorageBlock(new ResourceLocation(Almura.ID, "storage/quantum_chest"), 54);
+    public static final StorageBlock OAK_STORAGE_CRATE = new StorageBlock(new ResourceLocation(Almura.ID, "storage/oak_storage_crate"), 54);
+    public static final StorageBlock SPRUCE_STORAGE_CRATE = new StorageBlock(new ResourceLocation(Almura.ID, "storage/spruce_storage_crate"), 54);
+    public static final StorageBlock BITCH_STORAGE_CRATE = new StorageBlock(new ResourceLocation(Almura.ID, "storage/birch_storage_crate"), 54);
 }
