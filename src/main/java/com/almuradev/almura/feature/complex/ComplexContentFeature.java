1ab3f1627c7b5539755b7dd50fe22eb0638ab30a
Dockter <dockter@almuramc.com>
2018-03-29 23:05:28 -0500

    Implement ability to remove already registered vanilla recipes. * removed Stone Brick stair recipe and handled it within the loader.
    Signed-off-by: Dockter <dockter@almuramc.com>

--- src/main/java/com/almuradev/almura/feature/complex/ComplexContentFeature.java
+++ src/main/java/com/almuradev/almura/feature/complex/ComplexContentFeature.java
@@ -11,8 +11,11 @@ import com.almuradev.almura.feature.complex.item.almanac.item.FarmersAlmanacItem
 import com.almuradev.almura.feature.complex.item.wand.LightRepairWand;
 import com.almuradev.core.event.Witness;
 import net.minecraft.item.Item;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.registries.IForgeRegistryModifiable;
 
 public final class ComplexContentFeature implements Witness {
 
@@ -20,5 +23,12 @@ public final class ComplexContentFeature implements Witness {
     public void onRegisterItem(RegistryEvent.Register<Item> event) {
         event.getRegistry().register(new LightRepairWand());
         event.getRegistry().register(new FarmersAlmanacItem());
+    }
+
+    @SubscribeEvent
+    public void registerRecipes(RegistryEvent.Register<IRecipe> event) {
+        ResourceLocation StoneBrick = new ResourceLocation("minecraft:stone_brick_stairs");
+        IForgeRegistryModifiable modRegistry = (IForgeRegistryModifiable) event.getRegistry();
+        modRegistry.remove(StoneBrick); // Remove Vanilla Stock Brick recipe because it includes 3 variants of stone brick which is just stupid.
     }
 }
