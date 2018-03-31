c6e92fc110b6ebd78391d3cc8ffc8c8d2e1e17f8
Dockter <dockter@almuramc.com>
2018-03-31 00:06:50 -0500

    Put the correct Vanilla recipe back so it doesn't break Advancements.
    Signed-off-by: Dockter <dockter@almuramc.com>

--- src/main/java/com/almuradev/almura/feature/complex/ComplexContentFeature.java
+++ src/main/java/com/almuradev/almura/feature/complex/ComplexContentFeature.java
@@ -10,12 +10,16 @@ package com.almuradev.almura.feature.complex;
 import com.almuradev.almura.feature.complex.item.almanac.item.FarmersAlmanacItem;
 import com.almuradev.almura.feature.complex.item.wand.LightRepairWand;
 import com.almuradev.core.event.Witness;
+import net.minecraft.client.Minecraft;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.registries.IForgeRegistryModifiable;
 
 public final class ComplexContentFeature implements Witness {
@@ -31,5 +35,14 @@ public final class ComplexContentFeature implements Witness {
         ResourceLocation StoneBrick = new ResourceLocation("minecraft:stone_brick_stairs");
         IForgeRegistryModifiable modRegistry = (IForgeRegistryModifiable) event.getRegistry();
         modRegistry.remove(StoneBrick); // Remove Vanilla Stock Brick recipe because it includes 3 variants of stone brick which is just stupid.
+
+        GameRegistry.addShapedRecipe(new ResourceLocation("minecraft:stone_brick_stairs"), new ResourceLocation("minecraft:stone_brick_stairs"), new
+                        ItemStack(Blocks.STONE_BRICK_STAIRS),
+                "AXX",
+                "AAX",
+                "AAA",
+                'A', Blocks.STONEBRICK,
+                'X', Blocks.AIR
+        );
     }
 }
