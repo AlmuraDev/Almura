4aaaec6fd3ba2cdb4e5c97061295d9e321ff1ea1
Chris Sanders <zidane@spongepowered.org>
2018-04-10 22:38:23 -0500

    This can be done better.
    Signed-off-by: Chris Sanders <zidane@spongepowered.org>

--- src/main/java/com/almuradev/almura/feature/perms/PermsFeature.java
+++ src/main/java/com/almuradev/almura/feature/perms/PermsFeature.java
@@ -10,8 +10,8 @@ package com.almuradev.almura.feature.perms;
 import com.almuradev.almura.feature.title.ServerTitleManager;
 import com.almuradev.core.event.Witness;
 import me.lucko.luckperms.api.LuckPermsApi;
+import me.lucko.luckperms.api.User;
 import me.lucko.luckperms.api.event.user.track.UserTrackEvent;
-import org.spongepowered.api.Sponge;
 import org.spongepowered.api.entity.Transform;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
@@ -19,16 +19,17 @@ import org.spongepowered.api.event.Order;
 import org.spongepowered.api.event.entity.MoveEntityEvent;
 import org.spongepowered.api.event.filter.Getter;
 import org.spongepowered.api.event.service.ChangeServiceProviderEvent;
-import org.spongepowered.api.service.ProviderRegistration;
 import org.spongepowered.api.world.World;
-
-import java.util.Optional;
 
 import javax.inject.Inject;
 
 public final class PermsFeature implements Witness {
 
+  private static final String LUCK_PERMS_DEFAULT_GROUP = "default";
+
   private final ServerTitleManager titleManager;
+
+  private LuckPermsApi api;
 
   @Inject
   public PermsFeature(final ServerTitleManager titleManager) {
@@ -38,8 +39,8 @@ public final class PermsFeature implements Witness {
   @Listener
   public void onServiceChange(ChangeServiceProviderEvent event) {
     if (event.getNewProviderRegistration().getService().equals(LuckPermsApi.class)) {
-      final LuckPermsApi api = (LuckPermsApi) event.getNewProviderRegistration().getProvider();
-      api.getEventBus().subscribe(UserTrackEvent.class, e -> {
+      this.api = (LuckPermsApi) event.getNewProviderRegistration().getProvider();
+      this.api.getEventBus().subscribe(UserTrackEvent.class, e -> {
         this.titleManager.recalculateSelectedTitles();
         this.titleManager.refreshSelectedTitles();
       });
@@ -47,12 +48,11 @@ public final class PermsFeature implements Witness {
   }
 
   @Listener(order = Order.LAST)
-  public void playerMove(final MoveEntityEvent.Teleport event, @Getter("getTargetEntity") final Player player) throws IllegalAccessException {
-    if (differentExtent(event.getFromTransform(), event.getToTransform())) {
-      Optional<ProviderRegistration<LuckPermsApi>> provider = Sponge.getServiceManager().getRegistration(LuckPermsApi.class);
-      if (provider.isPresent()) {
-        final LuckPermsApi api = provider.get().getProvider();
-        if (api.getUser(player.getUniqueId()).getPrimaryGroup().equalsIgnoreCase("default")) { //LP's default group for new users.
+  public void playerMove(final MoveEntityEvent.Teleport event, @Getter("getTargetEntity") final Player player) {
+    if (this.api != null && differentExtent(event.getFromTransform(), event.getToTransform())) {
+      final User user = this.api.getUser(player.getUniqueId());
+      if (user != null) {
+      if (user.getPrimaryGroup().equalsIgnoreCase(LUCK_PERMS_DEFAULT_GROUP)) {
           // ToDo:  not ready to be implemented but is functional.
           // api.getUser(player.getUniqueId()).setPrimaryGroup("citizen");
         }
@@ -60,8 +60,7 @@ public final class PermsFeature implements Witness {
     }
   }
 
-
   private static boolean differentExtent(final Transform<World> from, final Transform<World> to) {
-    return !from.getExtent().equals(to.getExtent());
+    return !from.getExtent().getUniqueId().equals(to.getExtent().getUniqueId());
   }
 }
