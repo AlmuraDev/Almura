d6f93e67bc210eab5b21703a68cbf116ab774a99
Dockter <dockter@almuramc.com>
2018-06-02 23:10:56 -0500

    Implement TitleGUI.
    Signed-off-by: Dockter <dockter@almuramc.com>

--- src/main/java/com/almuradev/almura/feature/title/ClientTitleManager.java
+++ src/main/java/com/almuradev/almura/feature/title/ClientTitleManager.java
@@ -10,6 +10,7 @@ package com.almuradev.almura.feature.title;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 import com.almuradev.core.event.Witness;
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.network.FMLNetworkEvent;
 import net.minecraftforge.fml.relauncher.Side;
@@ -27,6 +28,7 @@ import javax.inject.Singleton;
 public final class ClientTitleManager implements Witness {
 
     private final Map<UUID, String> selectedTitlesById = new HashMap<>();
+    public String temporaryTitle;
 
     @SubscribeEvent
     public void onClientConnectedToServerEvent(FMLNetworkEvent.ClientConnectedToServerEvent event) {
@@ -36,6 +38,12 @@ public final class ClientTitleManager implements Witness {
     @Nullable
     public String getTitle(UUID uniqueId) {
         checkNotNull(uniqueId);
+
+        // Return temporary title set within TitleGUI
+
+        if (temporaryTitle != null && !temporaryTitle.isEmpty() && uniqueId == Minecraft.getMinecraft().player.getUniqueID()) {
+            return temporaryTitle;
+        }
 
         final String title = this.selectedTitlesById.get(uniqueId);
         if (title == null) {
---
+++ src/main/java/com/almuradev/almura/feature/title/client/gui/TitleGUI.java
@@ -0,0 +1,162 @@
+package com.almuradev.almura.feature.title.client.gui;
+
+/*
+ * This file is part of Almura.
+ *
+ * Copyright (c) AlmuraDev <https://github.com/AlmuraDev/>
+ *
+ * All Rights Reserved.
+ */
+
+import com.almuradev.almura.feature.nick.ClientNickManager;
+import com.almuradev.almura.shared.client.ui.FontColors;
+import com.almuradev.almura.shared.client.ui.component.UIFormContainer;
+import com.almuradev.almura.shared.client.ui.component.button.UIButtonBuilder;
+import com.almuradev.almura.shared.client.ui.screen.SimpleScreen;
+import com.google.common.eventbus.Subscribe;
+import net.malisis.core.client.gui.Anchor;
+import net.malisis.core.client.gui.component.decoration.UILabel;
+import net.malisis.core.client.gui.component.interaction.UIButton;
+import net.malisis.core.client.gui.component.interaction.UITextField;
+import net.malisis.core.renderer.font.FontOptions;
+import net.minecraft.client.gui.inventory.GuiInventory;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
+import org.spongepowered.api.plugin.PluginContainer;
+import org.spongepowered.api.scheduler.Task;
+import org.spongepowered.api.text.Text;
+
+import java.util.function.Consumer;
+
+import javax.inject.Inject;
+
+@SideOnly(Side.CLIENT)
+public final class TitleGUI extends SimpleScreen {
+
+    private static final int innerPadding = 2;
+    private int lastUpdate = 0;
+    private boolean unlockMouse = true;
+    private UILabel titleLabel;
+    private UIFormContainer form;
+
+    private EntityPlayer player;
+
+
+    @Inject private static PluginContainer container;
+
+    public TitleGUI(EntityPlayer player) {
+        this.player = player;
+    }
+
+    @Override
+    public void construct() {
+        guiscreenBackground = false;
+        Keyboard.enableRepeatEvents(true);
+
+        // Master Pane
+        form = new UIFormContainer(this, 300, 250, "");
+        form.setAnchor(Anchor.CENTER | Anchor.MIDDLE);
+        form.setMovable(true);
+        form.setClosable(true);
+        form.setBorder(FontColors.WHITE, 1, 185);
+        form.setBackgroundAlpha(215);
+        form.setBottomPadding(3);
+        form.setRightPadding(3);
+        form.setTopPadding(20);
+        form.setLeftPadding(3);
+
+        titleLabel = new UILabel(this, "Titles");
+        titleLabel.setFontOptions(FontOptions.builder().from(FontColors.WHITE_FO).shadow(true).scale(1.1F).build());
+        titleLabel.setPosition(0, -15, Anchor.CENTER | Anchor.TOP);
+
+        // Player Render Area
+        final UIFormContainer playerArea = new UIFormContainer(this, 75, 85, "");
+        playerArea.setPosition(0, -10, Anchor.RIGHT | Anchor.MIDDLE);
+        playerArea.setMovable(false);
+        playerArea.setClosable(false);
+        playerArea.setBorder(FontColors.WHITE, 1, 185);
+        playerArea.setBackgroundAlpha(215);
+        playerArea.setBottomPadding(3);
+        playerArea.setRightPadding(3);
+        playerArea.setTopPadding(3);
+        playerArea.setLeftPadding(3);
+
+        // Nicknames List Area
+        final UIFormContainer listArea = new UIFormContainer(this, 217, 150, "");
+        listArea.setPosition(0, 5, Anchor.LEFT | Anchor.TOP);
+        listArea.setMovable(false);
+        listArea.setClosable(false);
+        listArea.setBorder(FontColors.WHITE, 1, 185);
+        listArea.setBackgroundAlpha(215);
+        listArea.setBottomPadding(3);
+        listArea.setRightPadding(3);
+        listArea.setTopPadding(3);
+        listArea.setLeftPadding(3);
+
+        // Close button
+        final UIButton buttonClose = new UIButtonBuilder(this)
+                .width(40)
+                .anchor(Anchor.BOTTOM | Anchor.RIGHT)
+                .text(Text.of("almura.guide.button.close"))
+                .listener(this)
+                .build("button.close");
+
+        form.add(titleLabel, listArea, playerArea, buttonClose);
+
+        addToScreen(form);
+    }
+
+    @Subscribe
+    public void onUIButtonClickEvent(UIButton.ClickEvent event) {
+        switch (event.getComponent().getName().toLowerCase()) {
+            case "button.close":
+                close();
+                break;
+        }
+    }
+
+    protected Consumer<Task> openWindow(String details) {  // Scheduler
+        return task -> {
+            // Nothing setup yet.
+        };
+    }
+
+    @Override
+    public void update(int mouseX, int mouseY, float partialTick) {
+        super.update(mouseX, mouseY, partialTick);
+
+        // Set location of Player Entity within Panel
+        int i = form.screenX()+210;
+        int j = form.screenY()+75;
+
+        // Draw Player
+        GuiInventory.drawEntityOnScreen(i + 51, j + 75, 30, (float)(i + 51) - mouseX, (float)(j + 75 - 50) - mouseY, this.mc.player);
+
+        if (unlockMouse && this.lastUpdate == 25) {
+            Mouse.setGrabbed(false); // Force the mouse to be visible even though Mouse.isGrabbed() is false.  //#BugsUnited.
+            unlockMouse = false; // Only unlock once per session.
+        }
+
+        if (++this.lastUpdate > 100) {}
+    }
+
+    @Override
+    protected void keyTyped(char keyChar, int keyCode) {
+        super.keyTyped(keyChar, keyCode);
+        this.lastUpdate = 0; // Reset the timer when key is typed.
+    }
+
+    @Override
+    protected void mouseClicked(int x, int y, int button) {
+        super.mouseClicked(x, y, button);
+        this.lastUpdate = 0; // Reset the timer when mouse is pressed.
+    }
+
+    @Override
+    public boolean doesGuiPauseGame() {
+        return false; // Can't stop the game otherwise the Sponge Scheduler also stops.
+    }
+}
