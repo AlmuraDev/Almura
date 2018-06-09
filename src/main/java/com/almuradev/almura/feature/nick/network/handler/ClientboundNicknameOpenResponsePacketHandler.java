27b1bc58239c30fc49e56e703db84aff5f8f9767
Steven Downer <grinch@outlook.com>
2018-06-08 23:23:04 -0500

    Add mock data and sorting support to Exchange
    - Add missing license headers
    
    Signed-off-by: Steven Downer <grinch@outlook.com>

--- src/main/java/com/almuradev/almura/feature/nick/network/handler/ClientboundNicknameOpenResponsePacketHandler.java
+++ src/main/java/com/almuradev/almura/feature/nick/network/handler/ClientboundNicknameOpenResponsePacketHandler.java
@@ -1,3 +1,10 @@
+/*
+ * This file is part of Almura.
+ *
+ * Copyright (c) AlmuraDev <https://github.com/AlmuraDev/>
+ *
+ * All Rights Reserved.
+ */
 package com.almuradev.almura.feature.nick.network.handler;
 
 import com.almuradev.almura.feature.nick.client.gui.NicknameGUI;
