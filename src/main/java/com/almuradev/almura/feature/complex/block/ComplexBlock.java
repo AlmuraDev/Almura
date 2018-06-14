5a8305760139f537e494a5dc5963a4c61149334a
Dockter <dockter@almuramc.com>
2018-06-13 20:26:51 -0500

    Start work on Coin Exchange.
    Signed-off-by: Dockter <dockter@almuramc.com>

--- src/main/java/com/almuradev/almura/feature/complex/block/ComplexBlock.java
+++ src/main/java/com/almuradev/almura/feature/complex/block/ComplexBlock.java
@@ -7,10 +7,25 @@
  */
 package com.almuradev.almura.feature.complex.block;
 
+import com.almuradev.almura.Almura;
+import com.almuradev.content.type.itemgroup.ItemGroup;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.util.ResourceLocation;
+import org.spongepowered.api.Sponge;
 
-public abstract class ComplexBlock extends Block {
+public final class ComplexBlock extends Block {
+
+    public ComplexBlock(ResourceLocation registryName) {
+        super(Material.GROUND);
+        this.setRegistryName(registryName);
+        this.setUnlocalizedName(registryName.getResourcePath().replace('/', '.'));
+        this.setHardness(3.0F);
+        this.setResistance(1000.0F);
+
+        Sponge.getRegistry().getType(ItemGroup.class, Almura.ID + ":machine").ifPresent((itemGroup) -> setCreativeTab((CreativeTabs) itemGroup));
+    }
 
     public ComplexBlock(Material materialIn) {
         super(materialIn);
---
+++ src/main/java/com/almuradev/almura/feature/complex/block/ComplexBlocks.java
@@ -0,0 +1,8 @@
+package com.almuradev.almura.feature.complex.block;
+
+import com.almuradev.almura.Almura;
+import net.minecraft.util.ResourceLocation;
+
+public class ComplexBlocks {
+    public static ComplexBlock COIN_EXCHANGE = new ComplexBlock(new ResourceLocation(Almura.ID, "machine/coin_exchange"));
+}
