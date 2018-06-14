ed00a58229cb2d9b5f1a72167d7cbc34ce835386
Dockter <dockter@almuramc.com>
2018-06-13 20:32:31 -0500

    These will need the ability to have TE's.
    Signed-off-by: Dockter <dockter@almuramc.com>

--- src/main/java/com/almuradev/almura/feature/complex/block/ComplexBlock.java
+++ src/main/java/com/almuradev/almura/feature/complex/block/ComplexBlock.java
@@ -9,25 +9,35 @@ package com.almuradev.almura.feature.complex.block;
 
 import com.almuradev.almura.Almura;
 import com.almuradev.content.type.itemgroup.ItemGroup;
-import net.minecraft.block.Block;
+import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
 import org.spongepowered.api.Sponge;
 
-public final class ComplexBlock extends Block {
+import javax.annotation.Nullable;
 
-    public ComplexBlock(ResourceLocation registryName) {
+public final class ComplexBlock extends BlockContainer {
+
+    public ComplexBlock(ResourceLocation registryName, float hardness, float resistance) {
         super(Material.GROUND);
         this.setRegistryName(registryName);
         this.setUnlocalizedName(registryName.getResourcePath().replace('/', '.'));
-        this.setHardness(3.0F);
-        this.setResistance(1000.0F);
+        this.setHardness(hardness);
+        this.setResistance(resistance);
 
         Sponge.getRegistry().getType(ItemGroup.class, Almura.ID + ":machine").ifPresent((itemGroup) -> setCreativeTab((CreativeTabs) itemGroup));
     }
 
     public ComplexBlock(Material materialIn) {
         super(materialIn);
+    }
+
+    @Nullable
+    @Override
+    public TileEntity createNewTileEntity(World worldIn, int meta) {
+        return null;
     }
 }
--- src/main/java/com/almuradev/almura/feature/complex/block/ComplexBlocks.java
+++ src/main/java/com/almuradev/almura/feature/complex/block/ComplexBlocks.java
@@ -4,5 +4,5 @@ import com.almuradev.almura.Almura;
 import net.minecraft.util.ResourceLocation;
 
 public class ComplexBlocks {
-    public static ComplexBlock COIN_EXCHANGE = new ComplexBlock(new ResourceLocation(Almura.ID, "machine/coin_exchange"));
+    public static ComplexBlock COIN_EXCHANGE = new ComplexBlock(new ResourceLocation(Almura.ID, "machine/coin_exchange"), 3F, 1000F);
 }
