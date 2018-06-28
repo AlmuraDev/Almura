292ba97ff29f0c56170e1c4c1c65f228f05da77f
Dockter <dockter@almuramc.com>
2018-06-27 23:14:52 -0500

    Implement UIComplexImage because I can.
    Signed-off-by: Dockter <dockter@almuramc.com>

---
+++ src/main/java/com/almuradev/almura/shared/client/ui/component/UIComplexImage.java
@@ -0,0 +1,43 @@
+/*
+ * This file is part of Almura.
+ *
+ * Copyright (c) AlmuraDev <https://github.com/AlmuraDev/>
+ *
+ * All Rights Reserved.
+ */
+package com.almuradev.almura.shared.client.ui.component;
+
+import net.malisis.core.client.gui.GuiRenderer;
+import net.malisis.core.client.gui.MalisisGui;
+import net.malisis.core.client.gui.component.decoration.UIImage;
+import net.minecraft.client.Minecraft;
+import net.minecraft.item.ItemStack;
+
+public class UIComplexImage extends UIImage {
+
+    private ItemStack itemStack;
+
+    public UIComplexImage(MalisisGui gui, ItemStack itemStack) {
+        super(gui, itemStack);
+        this.itemStack = itemStack;
+    }
+
+
+    @Override
+    public void drawForeground(GuiRenderer renderer, int mouseX, int mouseY, float partialTick)
+    {
+        if (itemStack != null) {
+            float originalZLevel = Minecraft.getMinecraft().getRenderItem().zLevel;  // Save existing value
+            Minecraft.getMinecraft().getRenderItem().zLevel = this.getZIndex();  // Set the value to the current zIndex of the current component.
+
+            if (Minecraft.getMinecraft().getRenderItem().zLevel == 0F) {  // Detect if the value is still zero, if so subtract 100F because RenderItem adds 100F to the value.
+            	Minecraft.getMinecraft().getRenderItem().zLevel = -100F;  // Set this to -100F because I said so, trust me, I'm a professional.
+            } else {
+                Minecraft.getMinecraft().getRenderItem().zLevel = this.getZIndex();  // This is fine if the set value was zero.
+            }
+
+            renderer.drawItemStack(itemStack);
+            Minecraft.getMinecraft().getRenderItem().zLevel = originalZLevel; // Revert Setting to prevent the rest of the order from getting messed up.
+        }
+    }
+}
