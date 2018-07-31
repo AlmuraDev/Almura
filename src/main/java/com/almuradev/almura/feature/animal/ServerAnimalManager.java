58e32b85542cc44f97238691c8fe1d204b73e38f
Chris Sanders <zidane@spongepowered.org>
2018-07-30 19:56:31 -0500

    Babies are hard.
    So this should work but Sponge needs to be able to have both parents in the cause for the SpawnEntityEvent. I'm going to ask gabizou to take a look at what can be done.
    
    Signed-off-by: Chris Sanders <zidane@spongepowered.org>

--- src/main/java/com/almuradev/almura/feature/animal/ServerAnimalManager.java
+++ src/main/java/com/almuradev/almura/feature/animal/ServerAnimalManager.java
@@ -8,49 +8,92 @@
 package com.almuradev.almura.feature.animal;
 
 import com.almuradev.core.event.Witness;
-import com.google.inject.Inject;
-import net.minecraft.entity.passive.EntityChicken;
-import net.minecraft.entity.passive.EntityCow;
-import net.minecraft.entity.passive.EntityHorse;
-import net.minecraft.entity.passive.EntityPig;
-import net.minecraft.entity.passive.EntitySheep;
-import org.spongepowered.api.Sponge;
 import org.spongepowered.api.data.key.Keys;
+import org.spongepowered.api.entity.Entity;
+import org.spongepowered.api.entity.EntityTypes;
 import org.spongepowered.api.entity.living.animal.Animal;
+import org.spongepowered.api.entity.living.animal.Chicken;
+import org.spongepowered.api.entity.living.animal.Cow;
+import org.spongepowered.api.entity.living.animal.Horse;
+import org.spongepowered.api.entity.living.animal.Pig;
+import org.spongepowered.api.entity.living.animal.Sheep;
 import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.cause.EventContextKeys;
+import org.spongepowered.api.event.cause.entity.spawn.SpawnType;
+import org.spongepowered.api.event.cause.entity.spawn.SpawnTypes;
 import org.spongepowered.api.event.entity.BreedEntityEvent;
+import org.spongepowered.api.event.entity.InteractEntityEvent;
 import org.spongepowered.api.event.entity.SpawnEntityEvent;
-import org.spongepowered.api.plugin.PluginContainer;
+import org.spongepowered.api.item.inventory.ItemStackSnapshot;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.format.TextColors;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.inject.Singleton;
+
+@Singleton
 public final class ServerAnimalManager extends Witness.Impl  {
 
-    @Inject
-    private PluginContainer container;
+    private final Map<UUID, ItemStackSnapshot> interactedBreeds = new HashMap<>();
 
     @Listener
-    public void onSpawnEntity(SpawnEntityEvent event) {
+    public void onInteractEntitySecondaryMainHand(final InteractEntityEvent.Secondary.MainHand event) {
+        if (this.canAdditionalSpawn(event.getTargetEntity())) {
+            final ItemStackSnapshot usedItem = event.getContext().get(EventContextKeys.USED_ITEM).orElse(null);
+
+            this.interactedBreeds.put(event.getTargetEntity().getUniqueId(), usedItem);
+        }
+    }
+
+    @Listener
+    public void onSpawnEntity(final SpawnEntityEvent event) {
+        final SpawnType spawnType = event.getContext().get(EventContextKeys.SPAWN_TYPE).orElse(null);
         event.getEntities().stream()
-                .filter(e -> e instanceof Animal)
-                .map(e -> (Animal) e)
-                .forEach(e -> {
-                    if (e instanceof EntityCow || e instanceof EntityChicken || e instanceof EntityHorse || e instanceof EntityPig || e instanceof EntitySheep) {
-                        Sponge.getScheduler().createTaskBuilder()
-                                .delayTicks(1) // Delay this because the animals age isn't set yet.
-                                .execute(() -> {
-                                            if (!e.isRemoved() && e.getAgeData().age().get() != 0) {
-                                                e.offer(Keys.DISPLAY_NAME, Text.of(TextColors.AQUA, e.getType().getTranslation().get()));
-                                                // Todo: The below bugs may affect this feature.
-                                                // Bug 1: bug here, using .getTranslation().get() for MoCreatures returns "unknown"
-                                                // Bug 2: e.getType().getName() returns a lowerCase name of animals.
-                                            }
-                                        }
-                                )
-                                .submit(this.container);
-                        e.offer(Keys.CUSTOM_NAME_VISIBLE, true);
+            .filter(e -> e instanceof Animal)
+            .map(e -> (Animal) e)
+            .forEach(e -> {
+                e.offer(Keys.CUSTOM_NAME_VISIBLE, true);
+
+                if (spawnType != null && spawnType == SpawnTypes.BREEDING) {
+
+
+                    if (this.colorifyNameplate(e)) {
+                        e.offer(Keys.DISPLAY_NAME, Text.of(TextColors.AQUA, e.getType().getTranslation().get()));
                     }
-                });
+
+                    final ItemStackSnapshot parentA;
+                    final ItemStackSnapshot parentB;
+
+                    final List<Animal> animals = event.getCause().allOf(Animal.class);
+                    if (animals.size() == 2) {
+                        parentA = this.interactedBreeds.remove(animals.get(0).getUniqueId());
+                        parentB = this.interactedBreeds.remove(animals.get(1).getUniqueId());
+
+                        if (parentA != null && parentB != null) {
+                            if (parentA.getType() == parentB.getType()) {
+                                final int spawnCount = this.getAdditionalSpawnCount(e, parentA);
+
+                                for (int i = 0; i < spawnCount; i++) {
+                                    final Animal other = (Animal) e.getWorld().createEntity(e.getType(), e.getLocation().getPosition());
+                                    other.offer(Keys.AGE, -24000);
+
+                                    if (this.colorifyNameplate(other)) {
+                                        other.offer(Keys.DISPLAY_NAME, Text.of(TextColors.AQUA, e.getType().getTranslation().get()));
+                                    }
+
+                                    event.getEntities().add(other);
+                                }
+                            }
+                        }
+                    }
+                } else if (this.colorifyNameplate(e) && e.getAgeData().age().get() != 0) {
+                    e.offer(Keys.DISPLAY_NAME, Text.of(TextColors.AQUA, e.getType().getTranslation().get()));
+                }
+            });
     }
 
     @Listener
@@ -69,5 +112,22 @@ public final class ServerAnimalManager extends Witness.Impl  {
     public void onBreedEntityBreed(final BreedEntityEvent.Breed event) {
         // Change Name color AFTER BRED
         event.getCause().allOf(Animal.class).forEach(a -> a.offer(Keys.DISPLAY_NAME, Text.of(TextColors.BLUE, a.getTranslation().get())));
+    }
+
+    private boolean colorifyNameplate(final Animal animal) {
+        return animal instanceof Cow || animal instanceof Chicken || animal instanceof Horse || animal instanceof Pig || animal instanceof Sheep;
+    }
+
+    private boolean canAdditionalSpawn(final Entity entity) {
+        return entity instanceof Cow;
+    }
+
+    private int getAdditionalSpawnCount(final Animal baby, final ItemStackSnapshot usedItem) {
+        // TODO Check the item and the animal's type to drill this down
+        if (baby.getType() == EntityTypes.COW) {
+            return 5;
+        }
+
+        return 0;
     }
 }
