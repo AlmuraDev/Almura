b3a588fdf5152278819a04b57f8e45daac5b284c
Dockter <dockter@almuramc.com>
2018-07-31 20:56:02 -0500

    This was not suppose to be here.
    Signed-off-by: Dockter <dockter@almuramc.com>

--- src/main/java/com/almuradev/almura/feature/notification/ServerNotificationManager.java
+++ src/main/java/com/almuradev/almura/feature/notification/ServerNotificationManager.java
@@ -10,20 +10,12 @@ package com.almuradev.almura.feature.notification;
 import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.base.Preconditions.checkState;
 
-import com.almuradev.almura.feature.nick.ServerNickManager;
 import com.almuradev.almura.feature.notification.network.ClientboundPlayerNotificationPacket;
-import com.almuradev.almura.feature.title.ServerTitleManager;
 import com.almuradev.almura.shared.network.NetworkConfig;
 import com.almuradev.core.event.Witness;
-import io.github.nucleuspowered.nucleus.api.service.NucleusNicknameService;
 import org.spongepowered.api.Game;
 import org.spongepowered.api.GameState;
-import org.spongepowered.api.Sponge;
 import org.spongepowered.api.entity.living.player.Player;
-import org.spongepowered.api.event.Listener;
-import org.spongepowered.api.event.Order;
-import org.spongepowered.api.event.filter.Getter;
-import org.spongepowered.api.event.network.ClientConnectionEvent;
 import org.spongepowered.api.network.ChannelBinding;
 import org.spongepowered.api.network.ChannelId;
 import org.spongepowered.api.text.Text;
@@ -36,16 +28,11 @@ public final class ServerNotificationManager extends Witness.Impl implements Wit
 
     public final Game game;
     private final ChannelBinding.IndexedMessageChannel network;
-    private final ServerTitleManager serverTitleManager;
-    private final ServerNickManager serverNickManager;
 
     @Inject
-    public ServerNotificationManager(final Game game, @ChannelId(NetworkConfig.CHANNEL)ChannelBinding.IndexedMessageChannel network,
-        ServerTitleManager serverTitleManager, ServerNickManager serverNickManager) {
+    public ServerNotificationManager(final Game game, @ChannelId(NetworkConfig.CHANNEL)ChannelBinding.IndexedMessageChannel network) {
         this.game = game;
         this.network = network;
-        this.serverTitleManager = serverTitleManager;
-        this.serverNickManager = serverNickManager;
     }
 
     @Override
@@ -68,37 +55,5 @@ public final class ServerNotificationManager extends Witness.Impl implements Wit
         checkNotNull(message);
 
         this.network.sendTo(player, new ClientboundPlayerNotificationPacket(title, message));
-    }
-
-    // when a player attempts to join the server...
-    @Listener(order = Order.LAST)
-    public void onPlayerJoin(final ClientConnectionEvent.Join event, @Getter("getTargetEntity") Player player) {
-        checkNotNull(player);
-
-        String displayName = serverNickManager.getNickname(player);
-        //String playerTitle = serverTitleManager.getSelectedTitleFor(player);
-
-        for (final Player players : Sponge.getServer().getOnlinePlayers()) {
-            if (players.equals(player)) {
-                continue;
-            }
-            this.sendPopupNotification(player, Text.of("Player Joined"), Text.of(displayName + " has joined the server"), 5);
-        }
-    }
-
-    // when a player attempts to join the server...
-    @Listener(order = Order.PRE)
-    public void onPlayerQuit(final ClientConnectionEvent.Disconnect event, @Getter("getTargetEntity") Player player) {
-        checkNotNull(player); // I hope that this fire before our nickname or playername disappears.
-
-        String displayName = serverNickManager.getNickname(player);
-        //String playerTitle = serverTitleManager.getSelectedTitleFor(player);
-
-        for (final Player players : Sponge.getServer().getOnlinePlayers()) {
-            if (players.equals(player)) {
-                continue;
-            }
-            this.sendPopupNotification(player, Text.of("Player Disconnected"), Text.of(displayName + " has left the server"), 5);
-        }
     }
 }
