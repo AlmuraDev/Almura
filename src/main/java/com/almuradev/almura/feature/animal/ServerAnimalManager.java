f04b7e2ab86d78ed60823a18f92ffa0d34893271
Dockter <dockter@almuramc.com>
2018-08-02 22:09:18 -0500

    Did a SpongeForge change break this? See todos.
    Signed-off-by: Dockter <dockter@almuramc.com>

--- src/main/java/com/almuradev/almura/feature/animal/ServerAnimalManager.java
+++ src/main/java/com/almuradev/almura/feature/animal/ServerAnimalManager.java
@@ -9,11 +9,13 @@ package com.almuradev.almura.feature.animal;
 
 import com.almuradev.core.event.Witness;
 import com.google.inject.Inject;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.passive.EntityChicken;
 import net.minecraft.entity.passive.EntityCow;
 import net.minecraft.entity.passive.EntityHorse;
 import net.minecraft.entity.passive.EntityPig;
 import net.minecraft.entity.passive.EntitySheep;
+import org.apache.commons.lang3.text.WordUtils;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.data.key.Keys;
 import org.spongepowered.api.entity.living.animal.Animal;
@@ -37,10 +39,11 @@ public final class ServerAnimalManager extends Witness.Impl  {
                 .forEach(e -> {
                     if (e instanceof EntityCow || e instanceof EntityChicken || e instanceof EntityHorse || e instanceof EntityPig || e instanceof EntitySheep) {
                         Sponge.getScheduler().createTaskBuilder()
-                                .delayTicks(1) // Delay this because the animals age isn't set yet.
+                                .delayTicks(5) // Delay this because the animals age isn't set yet.
                                 .execute(() -> {
-                                            if (!e.isRemoved() && e.getAgeData().age().get() != 0) {
-                                                e.offer(Keys.DISPLAY_NAME, Text.of(TextColors.AQUA, e.getType().getTranslation().get()));
+                                    System.out.println("Spawn: " + e.getType().getName() + " / " + ((Entity)e).getName());
+                                    if (!e.isRemoved() && e.getAgeData().age().get() != 0) {
+                                                e.offer(Keys.DISPLAY_NAME, Text.of(TextColors.AQUA, ((Entity)e).getName()));
                                                 // Todo: The below bugs may affect this feature.
                                                 // Bug 1: bug here, using .getTranslation().get() for MoCreatures returns "unknown"
                                                 // Bug 2: e.getType().getName() returns a lowerCase name of animals.
@@ -56,18 +59,26 @@ public final class ServerAnimalManager extends Witness.Impl  {
     @Listener
     public void onBreedEntityReadyToMate(final BreedEntityEvent.ReadyToMate event) {
         final Animal animal = event.getTargetEntity();
-        animal.offer(Keys.DISPLAY_NAME, Text.of(animal.getTranslation().get()));  // Revert name to White
+        animal.offer(Keys.DISPLAY_NAME, Text.of(((Entity) animal).getName())); // Revert name to White
     }
 
     @Listener
     public void onBreedEntityFindMate(final BreedEntityEvent.FindMate event) {
         // Change Name to Yellow while animal is looking for mate.
-        event.getCause().allOf(Animal.class).forEach(a -> a.offer(Keys.DISPLAY_NAME, Text.of(TextColors.YELLOW, a.getTranslation().get())));
+        event.getCause().allOf(Animal.class).forEach(a -> {
+            a.offer(Keys.DISPLAY_NAME, Text.of(TextColors.YELLOW, ((Entity)a).getName()));
+            System.out.println("Find Mate: " + a.toString());
+        });
     }
 
     @Listener
     public void onBreedEntityBreed(final BreedEntityEvent.Breed event) {
         // Change Name color AFTER BRED
-        event.getCause().allOf(Animal.class).forEach(a -> a.offer(Keys.DISPLAY_NAME, Text.of(TextColors.BLUE, a.getTranslation().get())));
+        System.out.println("Fired Breed");
+        event.getCause().allOf(Animal.class).forEach(a -> {
+            // Todo: this no longer works.  Recreate by spawning two cows, mate them, this fires and doesn't change the name blue.
+            a.offer(Keys.DISPLAY_NAME, Text.of(TextColors.BLUE, ((Entity)a).getName()));
+            System.out.println("Breed Mate: " + a.toString());
+        });
     }
 }
