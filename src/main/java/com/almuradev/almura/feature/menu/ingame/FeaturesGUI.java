15c7190dc3b64f2a5a24b9f06163cc77f3edcd51
Dockter <dockter@almuramc.com>
2018-08-28 20:21:52 -0500

    Cleanup FeaturesGUI.
    Signed-off-by: Dockter <dockter@almuramc.com>

--- src/main/java/com/almuradev/almura/feature/menu/ingame/FeaturesGUI.java
+++ src/main/java/com/almuradev/almura/feature/menu/ingame/FeaturesGUI.java
@@ -11,22 +11,24 @@ import com.almuradev.almura.feature.claim.ClientClaimManager;
 import com.almuradev.almura.feature.exchange.client.ClientExchangeManager;
 import com.almuradev.almura.feature.guide.ClientPageManager;
 import com.almuradev.almura.feature.nick.ClientNickManager;
-import com.almuradev.almura.feature.store.client.gui.StoreScreen;
 import com.almuradev.almura.feature.store.client.gui.StoreListScreen;
 import com.almuradev.almura.feature.title.ClientTitleManager;
+import com.almuradev.almura.shared.client.ui.FontColors;
 import com.almuradev.almura.shared.client.ui.component.UIForm;
 import com.almuradev.almura.shared.client.ui.component.button.UIButtonBuilder;
 import com.almuradev.almura.shared.client.ui.screen.SimpleScreen;
 import com.google.common.eventbus.Subscribe;
 import net.malisis.core.client.gui.Anchor;
+import net.malisis.core.client.gui.component.decoration.UILabel;
+import net.malisis.core.client.gui.component.decoration.UISeparator;
 import net.malisis.core.client.gui.component.interaction.UIButton;
+import net.malisis.core.renderer.font.FontOptions;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
-import org.spongepowered.api.plugin.PluginContainer;
 import org.spongepowered.api.text.Text;
 
 import javax.inject.Inject;
@@ -37,8 +39,7 @@ public final class FeaturesGUI extends SimpleScreen {
   private int lastUpdate = 0;
   private boolean unlockMouse = true;
   private boolean isAdmin;
-  private UIButton guideButton, manageTitleButton, nicknameButton, manageExchangeButton, specificExchangeButton, serverShopButton, npcShopButton,
-    accessoriesButton, claimButton;
+  private UIButton guideButton, titleButton, nicknameButton, manageExchangeButton, manageStoreButton, accessoriesButton, claimButton;
 
   private World world;
   private EntityPlayerSP player;
@@ -61,103 +62,102 @@ public final class FeaturesGUI extends SimpleScreen {
     Keyboard.enableRepeatEvents(true);
 
     // Main Panel
-    final UIForm form = new UIForm(this, 150, 230, "Almura Features");
+    final UIForm form = new UIForm(this, 150, 220, "Almura Features");
+
+    if (!isAdmin) {
+      form.setSize(form.getWidth(), 150);
+    }
 
     // Guide button
     guideButton = new UIButtonBuilder(this)
-      .width(100)
-      .anchor(Anchor.TOP | Anchor.CENTER)
-      .text("Guide")
-      .listener(this)
-      .build("button.guide");
-
-    // Nickname button
-    nicknameButton = new UIButtonBuilder(this)
-      .width(100)
-      .anchor(Anchor.TOP | Anchor.CENTER)
-      .text("Nicknames")
-      .position(0, guideButton.getY() + 18)
-      .listener(this)
-      .build("button.nickname");
-
-    // Manage Titles button
-    manageTitleButton = new UIButtonBuilder(this)
-      .width(100)
-      .anchor(Anchor.TOP | Anchor.CENTER)
-      .text("Manage Titles")
-      .position(0, nicknameButton.getY() + 18)
-      .listener(this)
-      .build("button.title.manage");
-
-    // Manage Exchange button
-    manageExchangeButton = new UIButtonBuilder(this)
-      .width(100)
-      .anchor(Anchor.TOP | Anchor.CENTER)
-      .position(0, manageTitleButton.getY() + 18)
-      .visible(isAdmin)
-      .text("Manage Exchanges")
-      .listener(this)
-      .build("button.exchange.manage");
-
-    // Specific Exchange button
-    specificExchangeButton = new UIButtonBuilder(this)
-      .width(100)
-      .anchor(Anchor.TOP | Anchor.CENTER)
-      .position(0, manageExchangeButton.getY() + 18)
-      .visible(isAdmin)
-      .text("Global Exchange")
-      .listener(this)
-      .build("button.exchange.specific");
-
-    // Server Shop Configuration GUI button
-    serverShopButton = new UIButtonBuilder(this)
-      .width(100)
-      .anchor(Anchor.TOP | Anchor.CENTER)
-      .position(0, specificExchangeButton.getY() + 18)
-      .visible(isAdmin)
-      .text("Server Shops")
-      .listener(this)
-      .build("button.servershop");
-
-    // Server Shop - NPC Shop GUI button
-    npcShopButton = new UIButtonBuilder(this)
-      .width(100)
-      .anchor(Anchor.TOP | Anchor.CENTER)
-      .position(0, serverShopButton.getY() + 18)
-      .visible(isAdmin)
-      .text("NPC Shop")
-      .listener(this)
-      .build("button.npcshop");
-
-    // Accessories button
-    accessoriesButton = new UIButtonBuilder(this)
-      .width(100)
-      .anchor(Anchor.TOP | Anchor.CENTER)
-      .position(0, npcShopButton.getY() + 18)
-      .text("Accessories")
-      .visible(isAdmin)
-      .listener(this)
-      .build("button.accessories");
+        .width(100)
+        .anchor(Anchor.TOP | Anchor.CENTER)
+        .text("Guide")
+        .position(0, 4)
+        .listener(this)
+        .build("button.guide");
 
     // Claims button
     claimButton = new UIButtonBuilder(this)
-      .width(100)
-      .anchor(Anchor.TOP | Anchor.CENTER)
-      .position(0, accessoriesButton.getY() + 18)
-      .text("Claim Management")
-      .listener(this)
-      .build("button.claim");
+        .width(100)
+        .anchor(Anchor.TOP | Anchor.CENTER)
+        .position(0, guideButton.getY() + 18)
+        .text("Claim Management")
+        .listener(this)
+        .build("button.claim");
+
+    // Nickname button
+    nicknameButton = new UIButtonBuilder(this)
+        .width(100)
+        .anchor(Anchor.TOP | Anchor.CENTER)
+        .text("Nicknames")
+        .position(0, claimButton.getY() + 18)
+        .listener(this)
+        .build("button.nickname");
+
+    // Manage Titles button
+    titleButton = new UIButtonBuilder(this)
+        .width(100)
+        .anchor(Anchor.TOP | Anchor.CENTER)
+        .text("Titles")
+        .position(0, nicknameButton.getY() + 18)
+        .listener(this)
+        .build("button.title.manage");
+
+    // Accessories button
+    accessoriesButton = new UIButtonBuilder(this)
+        .width(100)
+        .anchor(Anchor.TOP | Anchor.CENTER)
+        .position(0, titleButton.getY() + 18)
+        .text("Accessories")
+        .enabled(isAdmin)
+        .listener(this)
+        .build("button.accessories");
+
+    final UISeparator functionsSeparator = new UISeparator(this);
+    functionsSeparator.setSize(form.getWidth(), 1);
+    functionsSeparator.setPosition(0, accessoriesButton.getY() + 22, Anchor.TOP | Anchor.CENTER);
+    functionsSeparator.setVisible(isAdmin);
+
+    final UILabel adminLabel = new UILabel(this, "Administrator");
+    adminLabel.setFontOptions(FontOptions.builder().from(FontColors.WHITE_FO).shadow(true).scale(1.1F).build());
+    adminLabel.setPosition(0, functionsSeparator.getY() + 4, Anchor.TOP | Anchor.CENTER);
+    adminLabel.setVisible(isAdmin);
+
+    final UISeparator functionsSeparator2 = new UISeparator(this);
+    functionsSeparator2.setSize(form.getWidth(), 1);
+    functionsSeparator2.setPosition(0, accessoriesButton.getY() + 37, Anchor.TOP | Anchor.CENTER);
+    functionsSeparator2.setVisible(isAdmin);
+
+    // Manage Exchange button
+    manageExchangeButton = new UIButtonBuilder(this)
+        .width(125)
+        .anchor(Anchor.TOP | Anchor.CENTER)
+        .position(0, functionsSeparator2.getY() + 10)
+        .visible(isAdmin)
+        .text("Manage Exchanges")
+        .listener(this)
+        .build("button.exchange.manage");
+
+    // Server Shop Configuration GUI button
+    manageStoreButton = new UIButtonBuilder(this)
+        .width(125)
+        .anchor(Anchor.TOP | Anchor.CENTER)
+        .position(0, manageExchangeButton.getY() + 18)
+        .visible(isAdmin)
+        .text("Manage Shops")
+        .listener(this)
+        .build("button.shop.manage");
 
     // Close button
     final UIButton buttonClose = new UIButtonBuilder(this)
-      .width(40)
-      .anchor(Anchor.BOTTOM | Anchor.RIGHT)
-      .text(Text.of("almura.button.close"))
-      .listener(this)
-      .build("button.close");
+        .width(40)
+        .anchor(Anchor.BOTTOM | Anchor.RIGHT)
+        .text(Text.of("almura.button.close"))
+        .listener(this)
+        .build("button.close");
 
-    form.add(guideButton, specificExchangeButton, manageExchangeButton, manageTitleButton, nicknameButton, accessoriesButton,
-      serverShopButton, npcShopButton, claimButton, buttonClose);
+    form.add(guideButton, manageExchangeButton, titleButton, nicknameButton, functionsSeparator, adminLabel, functionsSeparator2, accessoriesButton, manageStoreButton, claimButton, buttonClose);
 
     addToScreen(form);
   }
@@ -169,13 +169,13 @@ public final class FeaturesGUI extends SimpleScreen {
         exchangeManager.requestExchangeManageGui();
         break;
       case "button.exchange.specific":
-        exchangeManager.requestExchangeSpecificGui("almura.exchange.global");
+        //exchangeManager.requestExchangeSpecificGui("almura.exchange.global");
         break;
-      case "button.npcshop":
+      case "button.shop.specific":
         // Todo: need packet based request here.
-        new StoreScreen(true).display(); //isAdmin TRUE
+        //new StoreScreen(true).display(); //isAdmin TRUE
         break;
-      case "button.servershop":
+      case "button.shop.manage":
         // Todo: need packet based request here.
         new StoreListScreen().display();
         break;
