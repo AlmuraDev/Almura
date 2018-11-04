2b563e206772228423233615b9719f98de4b4133
Dockter <dockter@almuramc.com>
2018-11-03 19:26:01 -0500

    Fix license headers and bump to b60 for deployment.
    Signed-off-by: Dockter <dockter@almuramc.com>

--- src/main/java/com/almuradev/almura/feature/animal/asm/mixin/entity/passive/MixinEntityCow.java
+++ src/main/java/com/almuradev/almura/feature/animal/asm/mixin/entity/passive/MixinEntityCow.java
@@ -1,3 +1,10 @@
+/*
+ * This file is part of Almura.
+ *
+ * Copyright (c) AlmuraDev <https://github.com/AlmuraDev/>
+ *
+ * All Rights Reserved.
+ */
 package com.almuradev.almura.feature.animal.asm.mixin.entity.passive;
 
 import com.google.common.collect.Sets;
