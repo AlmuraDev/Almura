a82ec7d2935c41a299fbf6f51de8cf6b1e1cbf63
Dockter <dockter@almuramc.com>
2020-08-17 18:30:39 -0500

    Hookup unfinished feature to prevent guide from opening if option is selected.
    Signed-off-by: Dockter <dockter@almuramc.com>

--- src/main/java/com/almuradev/almura/feature/guide/network/handler/ClientboundGuideOpenResponsePacketHandler.java
+++ src/main/java/com/almuradev/almura/feature/guide/network/handler/ClientboundGuideOpenResponsePacketHandler.java
@@ -7,10 +7,13 @@
  */
 package com.almuradev.almura.feature.guide.network.handler;
 
+import com.almuradev.almura.core.client.config.ClientConfiguration;
 import com.almuradev.almura.feature.guide.client.gui.GuidePageViewScreen;
 import com.almuradev.almura.feature.guide.network.ClientboundGuideOpenResponsePacket;
 import com.almuradev.almura.feature.guide.network.GuideOpenType;
 import com.almuradev.almura.shared.util.PacketUtil;
+import com.almuradev.toolbox.config.map.MappedConfiguration;
+import com.google.inject.Inject;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -20,10 +23,15 @@ import org.spongepowered.api.network.RemoteConnection;
 
 public final class ClientboundGuideOpenResponsePacketHandler implements MessageHandler<ClientboundGuideOpenResponsePacket> {
 
+    @Inject private static MappedConfiguration<ClientConfiguration> configAdapter;
     @SideOnly(Side.CLIENT)
     @Override
     public void handleMessage(ClientboundGuideOpenResponsePacket message, RemoteConnection connection, Platform.Type side) {
         if (side.isClient()) {
+            final ClientConfiguration configuration = configAdapter.get();
+            if (!configuration.general.displayGuideOnLogin && !message.requestedFromClient) {
+                return;
+            }
             if (PacketUtil.checkThreadAndEnqueue(Minecraft.getMinecraft(), message, this, connection, side)) {
                 if (message.type == GuideOpenType.PLAYER_LOGGED_IN && (!Minecraft.getMinecraft().isIntegratedServerRunning())) {
                     new GuidePageViewScreen(message.canAdd, message.canRemove, message.canModify).display();
